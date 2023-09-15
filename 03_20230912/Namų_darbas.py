673e8e3b89aeeb47d8dd682801a68211297ddb91
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-09-15 10:26:01 +0300

    Add files via upload

---
+++ 03_20230912/Namų_darbas.py
@@ -0,0 +1,10 @@
+#Dalybaos veiksmo atsakymas (dalinys) visuomet yra float tipo
+
+b = 0.1
+c = 0.5
+
+print('%.40f' % b) # Išbandykite įvarius print() funcijos float kontamųjų apvalinimo parametrus
+print('%.40f' % c)
+
+print(6 / 3)
+print(type(6 / 3))
