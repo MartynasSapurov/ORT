6c90f6f279b1a676049325c8b2c3ee9341afba66
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-09-20 11:49:27 +0300

    Add files via upload

---
+++ 05_20230919/Namu darbas.py
@@ -0,0 +1,22 @@
+"""
+1 Uždavinys
+Netaikydami ciklų (naudokite inetvalo operatorių [:]) išveskite pateiktą eilutę:
+1. išveskitę į ekraną originalią eilutę
+2. išvesdami kas antrą simbolį
+3. išvesdami simbolius priešinga eilės tvarka
+4. išvesdami priešinga eilės tvarka kas antrą simbolį
+"""
+my_string = "G'mar chatima tovah. A good final sealing."
+
+"""
+2 Uždavinys
+Taikydami range funkciją ir fir ciklą išveskite šio sąrašo narius:
+1. originalia eilės tvarka
+2. originalia eilės tvarka pirmus 10 narių
+3. originalia eilės tvarka paskutinius 10 narių
+4. kas antrą narį originalia eilės tvarka
+4. visus narius priešinga eilės tvarka
+"""
+my_tuple = ("First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"
+"Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth", "Sixteenth", "Seventeenth", "Eighteenth",
+"Nineteenth", "Twentieth")
