d3bdf001f22fb7d038e5469601ee21c16e02ed09
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-10-02 10:13:11 +0300

    Update Klases_darbas.py

--- 09_20231002/Klases_darbas.py
+++ 09_20231002/Klases_darbas.py
@@ -28,3 +28,6 @@ kiekį tame avilyje. Net jei avilio aukštis 1, meškiukui teks kopti ir leistis
 Pasitikrinkite. Jei s = 100, n = 5, o skaičiai aprašantys medžius yra tokie: 9 ir 16; 7 ir 15; 5 ir 14; 6 ir 12; 2 ir 10, tai
 atsakymas: 109 kg
 """
+a, b = input().split(',')
+print(a)
+print(b)
