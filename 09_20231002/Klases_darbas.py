a6c18dc1bd831e159b637d5a55bd9ff572ede5f2
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-10-02 09:36:07 +0300

    Update Klases_darbas.py

--- 09_20231002/Klases_darbas.py
+++ 09_20231002/Klases_darbas.py
@@ -1,3 +1,3 @@
 #3 Uzd.
-Parašykite programą, kuri nustatytų, ar duotas skaičius n yra pirminis (turi tik du daliklius: vienetą ir patį save).
-Pasitikrinkite. Jei n = 7 , tai atsakymas: 7 yra pirminis. Jei n = 15 , tai atsakymas: 15 yra sudetinis
+#Parašykite programą, kuri nustatytų, ar duotas skaičius n yra pirminis (turi tik du daliklius: vienetą ir patį save).
+#Pasitikrinkite. Jei n = 7 , tai atsakymas: 7 yra pirminis. Jei n = 15 , tai atsakymas: 15 yra sudetinis
