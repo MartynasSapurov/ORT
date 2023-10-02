37efc17c60af68e561e2c0590872bfc176e5e8a6
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-10-02 09:38:27 +0300

    Update Klases_darbas.py

--- 09_20231002/Klases_darbas.py
+++ 09_20231002/Klases_darbas.py
@@ -1,3 +1,16 @@
 #3 Uzd.
 #Parašykite programą, kuri nustatytų, ar duotas skaičius n yra pirminis (turi tik du daliklius: vienetą ir patį save).
 #Pasitikrinkite. Jei n = 7 , tai atsakymas: 7 yra pirminis. Jei n = 15 , tai atsakymas: 15 yra sudetinis
+
+#4 Uzd.
+Miške auga medžiai, kuriuose bitės kuria savo avilius. Meškiukas žino, jog kur aviliai – ten ir medus. Viena bėda –
+aviliai dažnai kabo aukštai, o kopdamas meškiukas devynis prakaitus išlieja. Užkopdamas ar nusileisdamas 1 metrą
+meškiukas numeta 1 kilogramą svorio. Tačiau pasiekęs avilį, jis priauga tiek svorio,kiek suvalgo medaus. O suvalgo
+tiek medaus, kiek tik randa. Meškiukas žino, kaip aukštai kabo avilys ir kiek kilogramų medaus jame yra. Parašykite
+programą, kuri suskaičiuotų, kiek daugiausiai meškiukas gali sverti po pasivaikščiojimo miške. Pirmiausia reikia
+įvesti sveikąjį skaičių s – meškiuko dabartinis svorį kilogramais, paskui – medžių su aviliais skaičių miške n, o tada
+– skaičius poras, kurios aprašo konkretų medį: pirmasis skaičius reiškia avilio aukštį medyje, antrasis – medaus
+kiekį tame avilyje. Net jei avilio aukštis 1, meškiukui teks kopti ir leistis 1 metrą.
+
+Pasitikrinkite. Jei s = 100, n = 5, o skaičiai aprašantys medžius yra tokie: 9 ir 16; 7 ir 15; 5 ir 14; 6 ir 12; 2 ir 10, tai
+atsakymas: 109 kg
