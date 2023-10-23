d557e0ab811991e6a808a3de0fcabb644cf1e356
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-10-23 14:09:03 +0300

    Update Namu_darbas.py

--- 15_20231023/Namu_darbas.py
+++ 15_20231023/Namu_darbas.py
@@ -1,4 +1,4 @@
-#Duotas žodynasm, sukurkite naują žodyna jo pagrindu, pasitelkdami sąrašų (žodynų) generatorių
+#Duotas žodynas, sukurkite naują žodyną jo pagrindu pasitelkdami sąrašų (žodynų) generatorių
 chess_players = {
     "Carlsen, Magnus": 1865,
     "Firouzja, Alireza": 2804,
@@ -7,9 +7,9 @@ chess_players = {
     "Nepomniachtchi, Ian": 2773
 }
 """
-1. Naujame =odyne sukesikite indeksus ir vertes vietomis
+1. Naujajame žodyne sukesikite indeksus ir vertes vietomis
 2. Į naująjį žodyną įtraukite tik tuos įrašus kurių vertės originaliame žodyne didesnės nei 2000
-3. Naujojo žodyno vertės turi susidėti ne iš pavardės ir vardo atskirtų kalbleliais, o iš pavardės ir pirmosios vardo raidės.
+3. Naujojo žodyno vertės turi susidėti ne iš pavardės ir vardo, atskirtų kalbleliais, o iš pavardės ir pirmosios vardo raidės.
 
 Rezultatas pasitikrinimui:
 {
