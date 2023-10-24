2a4f6f96ec92a968e41de6f9bedbb8601141f40d
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-10-24 09:25:07 +0300

    Update Klases_darbas.py

--- 16_20231024/Klases_darbas.py
+++ 16_20231024/Klases_darbas.py
@@ -1,8 +1,8 @@
-#1. 
-#1. 
-#1. 
-#1. 
-#1. 
+#1. Pasinaudoję kortežu my_tuple ir sąrašų generatoriaus konstrukcija sukurkite naują kortežą, kurį sudarys tik skaičiai besidalinantys iš trijų
+#2. Pasinaudoję žodynu my_dict ir sąrašų generatoriaus konstrukcija sukurkite naują žodyną kurio visos vertės besidalinančios iš 7 bus pakeltos kvadratu
+#3. Pasinaudoję žodynu my_dict sąrašų generatoriaus konstrukcija sukurkite naują žodyną kurio visos vertės pakeltos kvadratu, ių naujai sukurto žodyno sudarykite kortežą kurį sudarys tik lyginių indeksų vertės
+#4. 
+#5. 
 
 my_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 my_dict = {1: 1, 2: 2, 3: 3, 4: 4, 5:5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11, 12: 12, 13: 13, 14: 14, 15: 15, 16: 16, 17: 17, 18: 18, 19: 19, 20: 20, 21: 21}
