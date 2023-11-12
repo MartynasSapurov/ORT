d878afb891488d17dae761139412dc5d2976bcec
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-11-12 18:12:23 +0200

    Add files via upload

---
+++ 17_20231107/Namu_darbas.py
@@ -0,0 +1,24 @@
+#Duota simbolių eilutė, kiekvienas atlikte savo variantą:
+
+# Dina:
+# b'\xc5\xa0ven\xc4\x8dion\xc4\x97li\xc5\xb3'
+
+# Adam:
+# b'\xc5\xa0e\xc5\xa1iasde\xc5\xa1imt\xc4\x85j\xc4\xaf'
+
+# Airanas:
+# b'D\xc5\xabk\xc5\xa1t\xc5\xb3 \xc4\x85\xc5\xbeuolynas'
+
+#Donis:
+# b'\xc5\xbdvirg\xc5\xbed\xc5\xb3 e\xc5\xbeeras'
+
+# Mark:
+# b'S\xc4\x85\xc5\xa1lav\xc5\xb3 s\xc4\x85\xc5\xa1lavynas'
+
+#Donat:
+# b'\xc5\xbdie\xc5\xbeirb\xc5\xb3 s\xc4\x85ranka'
+
+#Iškoduokite šią eilute taikydami numatytąją kodavimo sistemą (codec)
+#Tuomet gautą rezultatą užkoduokite taikydami Latin4 kodavimo sistemą (codec)
+#Palyginkite pradinės Utf-8 užkoduotos simbolių eilutės ilgį su naujai užkoduotos simbolių eilutės ilgiu
+#Visus rezultatus atspausdinkite
