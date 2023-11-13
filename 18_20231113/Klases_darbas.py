5f3537b0845572d8588f4e8037d2244dfc7696da
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-11-13 08:27:29 +0200

    Add files via upload

---
+++ 18_20231113/Klases_darbas.py
@@ -0,0 +1,23 @@
+#Pateikta citata originalo kalba:
+
+original_text = "Лучше двадцать раз ошибиться в человеке, чем относиться с подозрением к каждому."
+
+#1 užduotis
+#Užkoduokite šią citatą dviem skirtingomis kodavimo sistemomis (codec): Utf-8 ir Koi8_r
+#rezultatus įrašykite į atskirus kintamuosius text_utf_8 ir text_koir_8
+#Išveskite visų kintamųjų reikšmes į ekrana, taip pat atspausdinkite jų ilgius
+
+#2 Atspausdinkite tekstą iškoduota netinkama kodavmo sistema (codec),
+#text_utf_8 iškoduokite taikydami Koi8_r, o text_koir_8 iškoduokite taikydami Utf-8 kodavimo sistemas.
+
+#3 užduotis
+#Atspausdinkite visą tekstą po vieną simbolį (ne baitą) iš pradžių originliu formatu,
+#po to užkodavę Utf-8 ir Koi8_r kodavimo sistemomis (codec)
+
+#4 užduotis
+#Sukurkite žodyną kurio indeksas būt šio teksto autorius orginalo kalba, o vertė - ši citata
+
+#5 užduotis
+#Iškoduokite pateikt tekstą b'\xee\xe6\xec \xe8\xe5\xe1' taikydami Latin4 ir Iso-8859-8 kodavimo sistemas (codec),
+#nustatykiyye kuri iš šių kodavimo sistemų yra tinkama, rezultatus išveskite į ekraną
+#užkoduokite gautą tesingą tekstą Utf-8 kodavimo sistema, rezultatą išveskite į ekraną
