f70396294b69d6db797da30644321e4ee3c7149f
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-11-20 14:56:53 +0200

    Add files via upload

---
+++ 21_20231120/Namu_darbas.py
@@ -0,0 +1,31 @@
+#1. Užduotis
+#Parašykite programą kuri perskaitytų duomenis iš failo "file0.txt" ir išvestu po vieną eilutę
+
+#2. Užduotis
+#Parašykite programą kuri suskaičiuotų kiek faile "file1.txt" yra eilučių neprasidedančių raide "T".
+
+#3. Užduotis
+#Parašykite programą kuri suskaičiuotų kiek faile "file1.txt" yra žodžių
+
+#4. Užduotis
+#Parašykite programą kuri suskaičiuotų kiek kartų faile "file1.txt" yra minimas žodis "the" arba "The".
+
+#5. Užduotis
+#Parašykite programą kuri suskaičiuotų kiek faile "file1.txt" yra žodžių trumpesnių nei 4 simboliai
+
+#6. Užduotis
+#parašykite programą kuri suskaičiuotų kiek faile "file2.txt" yra žodžių "this" and "these", atkreipkite dėmesį, kad
+#būtų skaičiuojami tik pilni žodžiai
+
+#7. Užduotis
+#parašykite programą kuri suskaičiuotų kiek faile "file.txt" yra žodžių užsibaigiančių raide "e"
+
+#8. Užduotis
+#parašykite programą kuri suskaičiuotų kiek faile "file.txt" yra didžiųjų raidžių
+
+#9. Užduotis
+#parašykite programą kuri nuskaitytų duomenis if "file2.txt" ir i6trintu visus $ ženklus
+
+#10. Užduotis
+#parašykite programą kuri nuskaituytų duomenis iš failo "file2.txt" ir nekeičiant failo atspausdintų duomenis
+#pakeičiant visus simbolius "oo" į "ū"
