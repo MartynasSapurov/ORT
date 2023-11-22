06b0d9b40320a05181841f315780553490e1c61e
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-11-22 15:14:30 +0200

    Add files via upload

---
+++ 22_20231121/Namu_darbas.py
@@ -0,0 +1,21 @@
+#1. Užduotis
+#Duotas json failas uzduotis.json
+#Nuskaitykite duomenis ir atspausdinkite
+
+#2. Užduotis
+#Atspausdinkite adresą ("address"), po to  telefono numerius ("phone_numbers"), po to vaikų sąrašą ("childdren")
+
+#3. Užduotis
+#Atspausdinkite namų telefono numerį ir jo tipą ("phone_numbers", "type": "home")
+
+#4. Užduotis
+#Visus nuskaitytus duomenis pakesikite į json eilutę taikydami funkciją json.dumps()
+#tuomet pakeiskite visus sibolius "[" į "(", o simbolius "]" į ")" ir atspausdinkite pakeistą eilutę
+
+#5. Užduotis
+#Įrašykite pateiktus duomenis į failą excel.csv
+name_of_field = ["Dešimtainis", "Dvejetainis"]
+fields = [["5",], ["7",],  ["16",], ["25",], ["100",], ["255",], ["41",], ["85",]]
+#Importuokite duomenis į Excel, tuomet pasitelkę Excel funkciją suskaičiukite dvejetaines vertes
+
+#Siųsdami namų darbą pridėkite json, csv ir xlsx failus, taip pat įkėliau užduočių atsakymus: 1_Uzduotis.png ....
