e438b2df7f3690be950d394f14d3ef82eba17827
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-12-04 18:08:31 +0200

    Update Namu_darbas.py

--- 24_20231204/Namu_darbas.py
+++ 24_20231204/Namu_darbas.py
@@ -4,4 +4,4 @@ import random #Įtraukiame random biblioteką
 random.randint(1, 20) #Sugeneruojame atsitiktinį sveikajį skačių nuo 1 iki 20
 
 #Parašykite ir išbandykite funkciją kuri funkcijos viduje paprašytų įvesti du kaičius ir grąžintų atsitiktinį skaičių
-#intervale tarp tų dviejų skaičių. Funkcija turite su kurti be argumentų
+#intervale tarp tų dviejų skaičių. Funkcija turite sukurti be argumentų
