b6810715be71698ceccee216bdee7e311ce71c65
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-12-04 18:00:53 +0200

    Create Namu_darbas.py

---
+++ 24_20231204/Namu_darbas.py
@@ -0,0 +1,7 @@
+#Random funkcijos naudojimas
+import random #Įtraukiame random biblioteką
+
+random.randint(1, 20) #Sugeneruojame atsitiktinį sveikajį skačių nuo 1 iki 20
+
+#Parašykite ir išbandykite funkciją kuri funkcijos viduje paprašytų įvesti du kaičius ir grąžintų atsitiktinį skaičių
+#intervale tarp tų dviejų skaičių. Funkcija turite su kurti be argumentų
