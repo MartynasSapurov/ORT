93261897dd511318f409af1d9417921c17801d35
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-01-15 09:38:37 +0200

    Update Klases_darbas.py

--- 28_20240115/Klases_darbas.py
+++ 28_20240115/Klases_darbas.py
@@ -1,4 +1,4 @@
-#1. Sugeneruokite 20 atsitiktinių skaičių ir įrašykite juos į failą data.txt
+#1. Sugeneruokite 20 atsitiktinių skaičių (tarp 1 ir 999) ir įrašykite juos į failą data.txt
 
 import random
 random.randint(1, 10) #Atsitiktinis kaičius tarp 1 ir 10
