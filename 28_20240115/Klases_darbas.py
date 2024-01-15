64c2650a81a4b93b04727279696fec2cb63e1847
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-01-15 09:46:46 +0200

    Update Klases_darbas.py

--- 28_20240115/Klases_darbas.py
+++ 28_20240115/Klases_darbas.py
@@ -1,4 +1,7 @@
 #1. Sugeneruokite 20 atsitiktinių skaičių (tarp 1 ir 999) ir įrašykite juos į failą data.txt
+#2. Nuskaitykite duomenis iš failo įkelite į sąrašą, surušiuokite didėjimo tvarką ir įrašykite į failą result.txt
+
+
 
 #Pavyzdys
 import random
