7eb527856477216ff7b75fb8806173f7824f6268
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-04-15 14:30:44 +0300

    Update Namu_darbas.py

--- 28_20240415/Namu_darbas.py
+++ 28_20240415/Namu_darbas.py
@@ -1,3 +1,3 @@
 #Sukurkite lambda funkciją, kuri nustatytų skaičius lyginis ar nelyginis
 #Funkcijos nuskaito vieną argumentą (skaičių), jei skaičius lyginis grąžina žodį "Lyginis", je ne - "Nelyginis",
-#jei skaičius lygus 0, grą-ina tekstą "Nulis"
+#jei skaičius lygus 0, grąžina tekstą "Nulis"
