3927758ada5bcbbaad9893d95ed43ec38c0fd560
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-04-15 10:08:42 +0300

    Create Klases_darbas.py

---
+++ 28_20240415/Klases_darbas.py
@@ -0,0 +1,16 @@
+def area(b, h):
+    return 0.5 * b * h
+
+
+area_2 = lambda b, h: 0.5 * b * h
+
+#area_2 = lambda b, h: 0.5 * b * h
+#print(area_2(2, 3))
+
+#Sukurkite lambda funkciją, kuri nuskaitys sąrašą
+# ir išves duomenis tokia forma “Hello, {name}” į kitą sąrašą
+# Vis pavadinimai privalo būti parašyti dižiąja raide
+
+#ame_list = ['Bob', 'jack', 'NENCI', 'EriC']
+#new_list = lambda x: [f'Hello, {name.title()}' for name in x]
+#print(new_list(name_list))
