639ad0e28dc42a847870762a46ffa04003e51353
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-04-16 10:17:23 +0300

    Update Klases_darbas.py

--- 28_20240416/Klases_darbas.py
+++ 28_20240416/Klases_darbas.py
@@ -1,3 +1,10 @@
 my_pets = ['alfred', 'tabitha', 'william', 'arla']
 
 #pritaikę map funkciją sukurkite naują sąrašą kuriame bus tik pirmosios trys vardų vaidės
+
+def my_fumc(x):
+  return x.upper()
+
+my_newlist[]
+
+my_newlist = list(map(my_fumc, my_pets))
