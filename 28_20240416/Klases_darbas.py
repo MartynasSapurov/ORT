2c001c3a082d858a91dad3eff3bd22f1521d3fd8
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-04-16 11:03:08 +0300

    Update Klases_darbas.py

--- 28_20240416/Klases_darbas.py
+++ 28_20240416/Klases_darbas.py
@@ -14,3 +14,5 @@ my_newlist = list(map(my_fumc, my_pets))
 #kuriame pirmasis elementas turės 1 skaičių po kalbelio, antras 2 ir tt.
 
 circle_areas = [3.567738, 5.576681, 4.009147, 56.242413, 9.013442, 32.000133]
+new_areas = list(map(round, circle_areas, [i for i in range(1, len(circle_areas)+1)]))
+print(new_areas)
