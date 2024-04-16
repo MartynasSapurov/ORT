2db9360b5d591d36a8ef8ab489a322610822d578
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-04-16 10:50:28 +0300

    Update Klases_darbas.py

--- 28_20240416/Klases_darbas.py
+++ 28_20240416/Klases_darbas.py
@@ -8,3 +8,8 @@ def my_fumc(x):
 my_newlist[]
 
 my_newlist = list(map(my_fumc, my_pets))
+
+#Duotas sąrašas taškų (skaičių)
+#pritaikę map() funkciją sukurkite naują sąrašą
+
+circle_areas = [3.567738, 5.576681, 4.009147, 56.242413, 9.013442, 32.000133]
