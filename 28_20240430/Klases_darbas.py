23ab2458b6afc0c3641aa42135063a11d0551c0f
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-04-30 10:06:42 +0300

    Update Klases_darbas.py

--- 28_20240430/Klases_darbas.py
+++ 28_20240430/Klases_darbas.py
@@ -2,7 +2,7 @@ my_pets = ['alfred', 'tabitha', 'william', 'arla']
 
 #pritaikę map funkciją sukurkite naują sąrašą kuriame bus tik pirmosios trys vardų vaidės
 
-my_upper_pets_4 = list(lambda my_pets: my_pets[:3].upper(), my_pets)
+my_upper_pets_4 = list(map(lambda my_pets: my_pets[:3].upper(), my_pets))
 
 print(my_upper_pets_4)
 
