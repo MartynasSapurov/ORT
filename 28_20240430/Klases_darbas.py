577ab62f36dce6013ac11c50e5c0939743a6dc83
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-04-30 10:29:44 +0300

    Update Klases_darbas.py

--- 28_20240430/Klases_darbas.py
+++ 28_20240430/Klases_darbas.py
@@ -1,3 +1,4 @@
+#1
 my_pets = ['alfred', 'tabitha', 'william', 'arla']
 
 #pritaikę map funkciją sukurkite naują sąrašą kuriame bus tik pirmosios trys vardų vaidės
@@ -9,7 +10,7 @@ print(my_upper_pets_4)
 #Pakeiskite lambda funkciją, paprasta funkcija
 
 
-
+#2
 #################################################################################
 
 scores = [66, 90, 68, 59, 76, 60, 88, 74, 81, 65, 75, 99, 12]
@@ -22,7 +23,8 @@ answer = list(filter(my_func, scores))
 
 print(answer)
 
-#Parašykite su lambda funkcija
+#Namuose parašykite su lambda funkcija
+#3
 #Taikydami map funkcija vardus saraše prasidedančius raide "a" pakeiskite visomis didžiosiomis raidėmis, o visus kitus užrašykite atbulai.
 
 my_pets = ['alfred', 'tabitha', 'william', 'arla']
