d766e005d349f444a872473deba70589beae8703
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-05-02 10:34:42 +0300

    Update and rename Klases_darbas.py to Klases_darbas.py

---
+++ 28_20240502/Klases_darbas.py
@@ -0,0 +1,20 @@
+#1
+
+Parašykite lambda funkcija kuri nuskaitytų duotą sąrašą ir jei pirmoji žodžio sąraše raide = 'a' arba 'A' grąžintų
+pasutinius tris simbolius didžiosiomis raidėmis, jei pirmoji radė nelygi 'a' arba 'A' grąžintų pirus tris simbolius mažosiomis raidėmis.
+
+['Albert', 'bob', 'Robert', 'aliona', 'Algirdas', 'alicija', 'UZDUOTIS', 'pirmSiS']
+
+#3
+Duotas s1rašas sudarytas iš skirtingų tipų duomenų.
+Taikydami map() funkciją parašykite programą kuri grąžintų sąrašą kuriame kiekvienas int tipo kintamasis būtų pakeistas į str tipą,
+visus kitų tipų elemntūs įkelkite į sąrašą nekeisdami. Duomenų pakeitimui map funkcijos visuje panaudokite lambda funkciją.
+
+values = [1, 2, '3', 'forth', 'end', 99, True, None]
+
+#3
+Taikydami filter() funkciją iš ždžių sąrašo išrinkite tikt tuos, kurie yra polindromai (skaitomi vienodai iš abiejų pusių),
+programa turi nekreipti dėmesio į tai raidės didžiosios ar mažosios. Duomenims atrinkti taikykite lambda funkcijas.
+
+
+inputdata = ['alus', 'eme', 'sava', 'valstybe', 'palapine', 'kroliai', 'sula', 'namai', 'kalba']
