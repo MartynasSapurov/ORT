37325af763723ef5568b92c49775213f92c138b7
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-05-28 07:58:17 +0300

    Update Klases_darbas.py

--- 33_20240528/Klases_darbas.py
+++ 33_20240528/Klases_darbas.py
@@ -1,1 +1,41 @@
+#1. Duotas sąrašas 
+noah_animals = [
+    "LiOn",
+    "TiGeR",
+    "ElEpHaNt",
+    "GiRaFfE",
+    "ZeBrA",
+    "KaNgArOo",
+    "PaNdA",
+    "KoAlA",
+    "RhInOcErOs",
+    "HiPpOpOtAmUs",
+    "BeAr",
+    "WoLf",
+    "LeOpArD",
+    "ChEeTaH",
+    "BuFfAlO",
+    "DeEr",
+    "CaMeL",
+    "OsTrIcH",
+    "PeNgUiN",
+    "FlAmInGo"
+]
+#pritaikę map ir lambda funkcijas sukurkite naują sąrašą kuriame visi lyginių laukelių gyvynai bus pateikti didžiosiomis o nelyginių mažosiomis raidėmis.
 
+#2. Taikydami filter funkciją sukurkite naują sarašą kuriame būtų pateikiami tik vardai prasidedantys "a" arba "A" raidėmis.
+jewish_names = [
+    "David", "Avraham", "Eli", "ari", "Jacob", "aTalia", "Noah", "aLiza",
+    "Samuel", "aMoshe", "Benjamin", "aLeah", "Isaac", "aHava", "Joseph", "aShira",
+    "Daniel", "aYakov", "Nathan", "aRiel"
+]
+
+#3. Taikydami map ir lambda  funkcijas perkelkite į naują sąrašą tik moksleivius kurių vidurkis didesnis arba lygus 8
+students = [
+    ("John", 8.8),
+    ("Emma", 7.2),
+    ("Olivia", 8.8),
+    ("Liam", 9.5),
+    ("Noah", 7.8),
+    ("Sophia", 9.2)
+] 
