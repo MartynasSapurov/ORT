ef5732f84b7eb977552d91416d3bfbcecfa5a643
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-09-16 09:42:35 +0300

    Update Namu_darbas.py

--- 10B/01_Pamoka_20240910/Namu_darbas.py
+++ 10B/01_Pamoka_20240910/Namu_darbas.py
@@ -1,1 +1,26 @@
-a
+#Namų darbas
+#Duoti trys kintamieji
+skaicius = 5
+trupmeninis_skaicius = 5.8
+tekstas = "Labas rytas"
+
+#Taikant type() ir id() funkcijas galima atspausdinti šių kintamųjų vertes, tipus ir adresus atmintyje
+print(skaicius, type(skaicius), id(skaicius))
+print("*"*80)
+print(trupmeninis_skaicius, type(trupmeninis_skaicius), id(trupmeninis_skaicius))
+print("*"*80)
+print(tekstas, type(tekstas), id(tekstas))
+print("*"*80)
+
+#Patobulinkite pateiktą kodo atkarpą taip, kad šie duomnys būtų spausdinami pateikiant aiškų ir rišlų tekstą
+#(atkreipkite dėmesį, kad norint atspausdint sibolį ", jis nurodomas su pasvyruoju brūkšneliu \")
+#rezultatas tururėtų atrodyti taip:"
+
+"""
+Kintamojo "skaičius" vertė = "5", tipas = "<class 'int'>", o adresas = "140722714909240"
+********************************************************************************
+Kintamojo "trupmeninis_skaicius" vertė = "5.8", tipas = "<class 'float'>", o adresas = "2458351086704"
+********************************************************************************
+Kintamojo "tekstas" vertė = "Labas rytas", tipas = "<class 'str'>", o adresas = "2458355027056"
+********************************************************************************
+"""
