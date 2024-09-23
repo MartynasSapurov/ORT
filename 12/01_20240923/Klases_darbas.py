090741c4ff8d16b8758d6ab9e28b91503e61782d
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-09-23 09:13:15 +0300

    Create Klases_darbas.py

---
+++ 12/01_20240923/Klases_darbas.py
@@ -0,0 +1,42 @@
+"""
+my_list = ['pirmas', 'antras', 'trecias', 'ketvirtas', 'ketvirtas']
+my_tuple = ('pirmas', 'antras', 'trecias', 'ketvirtas', 'ketvirtas')
+my_set = {'pirmas', 'antras', 'trecias', 'ketvirtas'}
+my_dict = {1:'pirmas', 2:'antras', 'geriausias_skaicius':'trecias', 4:'ketvirtas'}
+
+print(f"List is: {my_list} \nType is: {type(my_list)}")
+print("*"*80)
+print(my_tuple, type(my_tuple), sep='\n')
+print("*"*80)
+print(my_set, type(my_set), sep='\n')
+print("*"*80)
+print(my_dict, type(my_dict), sep='\n')
+print("*"*80)
+print(my_dict['geriausias_skaicius'])
+"""
+"""
+file = open('U1.txt', 'rt')
+my_line = file.readline()
+file.close()
+
+print(my_line)
+
+with open('U1.txt', 'rt') as file:
+    my_line = file.readline()
+
+print(my_line)
+
+try:
+    file = open('U1.txt', 'rt')
+    my_line = file.readline()
+finally:
+    file.close()
+
+print(my_line)
+"""
+#my_list = []
+with open('U1.txt', 'rt') as file:
+    n = int(file.readline())
+    my_list = file.readline().split()
+    my_list = [int(item) for item in my_list]
+    print(my_list)
