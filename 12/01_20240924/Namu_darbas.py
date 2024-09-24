d7bfa2ef3fdebeccfa6f6d1efa95a4f6110a4b29
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-09-24 11:11:17 +0300

    Create Namu_darbas.py

---
+++ 12/01_20240924/Namu_darbas.py
@@ -0,0 +1,20 @@
+"""
+with open('U1.txt', 'rt') as file:
+    my_list = [[int(item) for item in items.split()] for items in file.readlines()[1:]]
+    print(my_list)
+    #my_list = file.readline().split()
+    #my_list = [int(item) for item in my_list]
+    #print(my_list)
+"""
+"""
+line = []
+with open('U1.txt', 'rt') as file:
+    n=int(file.readline())
+    print(n)
+    for i in range(n):
+        line.append([int(item) for item in file.readline().split()])
+    print(line)
+"""
+#PPateikti du duomenų išrinkimo iš failo algoritmai, pasirinkite bet kurį. Pasinaudoję gautu dvimačiu masyvu (dviejų lygmenų sąrašu) sukurkite
+#naują sąrašą arba žodyną kuriame būtų pateikiama sumini statistika apie kiekvieną žadėją: žaidėjo numeris(kartojasi tik vieną kartą ir suminiai duomenys apie
+#visas rungtynes, taip pat pridėkite papildomą laukelį kuriame nurodomas sužaistų rungtynių skaičius.
