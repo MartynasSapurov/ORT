eab8339daa67c5071e4746d4476f4ac592c126cf
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-09-24 08:43:27 +0300

    Update Klases_darbas.py

--- 10B/03_Pamoka_20240924/Klases_darbas.py
+++ 10B/03_Pamoka_20240924/Klases_darbas.py
@@ -2,7 +2,16 @@
 1. Sraigė per valandą nušliaužia z žiogo šuolių. Žiogas vienu šuoliu nušoka c centimetrų. Kiek centimetrų
 sraigė nušliauš per v valandų? Parašykite programą šiam uždaviniui spręsti.
 Pasitikrinkite. Kai z = 2, c = 2, v = 8, tai turi būti spausdinama 32.
+"""
+z = int(input("Įveskite žiogo šuolių kiekį:\n")) #Nuskaitome žiogo šuolių kiekį
+c = int(input("Įveskite žiogo šuolių ilgį, cm:\n")) #Nuskaitome žiogo šuolio ilgį cm
+v = int(input("Įveskite kiek valandų šliauš sraigė:\n")) #Nuskaitome kiek valandų šliauš sraigė
 
+result = z*c*v
+
+print(f"Sraigė nušliauš {result} cm")
+
+"""
 2. Laivynas iš k laivų, kurių kiekvienas plukdė po n maišų prieskonių, pakliuvo į audrą, kurioje m laivų
 nuskendo, o kiti sėkmingai pasiekė kelionės tikslą. Kiek maišų prieskonių pavyko parvežti?
 Pasitikrinkite. Kai k = 15, n = 8000 , m = 3, tai pavyko parvežti 96000 maišų prieskonių.
@@ -10,7 +19,7 @@ Pasitikrinkite. Kai k = 15, n = 8000 , m = 3, tai pavyko parvežti 96000 maišų
 3. Per kiek laiko tamsiame urve paklydęs turistas vieną po kito sudegins n degtukų, jei kiekvienas iš jų dega m
 minučių? Atsakymą išreikškite valandomis ir minutėmis.
 Pasitikrinkite. Kai n = 75, m = 2 , tai degtukai sudegs per 2 val. ir 30 min.
-      
+
 4. Laikrodis rodo x valandų ir y minučių. Parašykite programą, kuri apskaičiuotų, kiek minučių minut ir
 kiek sekundžių sec prabėgo nuo vidurnakčio.
 Pasitikrinkite. Įvedę x = 3 ir y = 5, turėtumėte gauti: minut = 185, sec = 11100.
