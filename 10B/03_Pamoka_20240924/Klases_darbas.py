c9abcf5bf1885fdc83ee265c8e2931a1286c5f7d
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-09-24 08:35:39 +0300

    Update Klases_darbas.py

--- 10B/03_Pamoka_20240924/Klases_darbas.py
+++ 10B/03_Pamoka_20240924/Klases_darbas.py
@@ -1,3 +1,4 @@
+"""
 1. Sraigė per valandą nušliaužia z žiogo šuolių. Žiogas vienu šuoliu nušoka c centimetrų. Kiek centimetrų
 sraigė nušliauš per v valandų? Parašykite programą šiam uždaviniui spręsti.
 Pasitikrinkite. Kai z = 2, c = 2, v = 8, tai turi būti spausdinama 32.
@@ -13,3 +14,4 @@ Pasitikrinkite. Kai n = 75, m = 2 , tai degtukai sudegs per 2 val. ir 30 min.
 4. Laikrodis rodo x valandų ir y minučių. Parašykite programą, kuri apskaičiuotų, kiek minučių minut ir
 kiek sekundžių sec prabėgo nuo vidurnakčio.
 Pasitikrinkite. Įvedę x = 3 ir y = 5, turėtumėte gauti: minut = 185, sec = 11100.
+"""
