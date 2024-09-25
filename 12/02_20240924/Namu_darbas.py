aeac5e4a80d560e1ee09830e8144d739cdc3ad6d
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-09-25 09:40:25 +0300

    Update Namu_darbas.py

--- 12/02_20240924/Namu_darbas.py
+++ 12/02_20240924/Namu_darbas.py
@@ -16,5 +16,16 @@ with open('U1.txt', 'rt') as file:
     print(line)
 """
 #PPateikti du duomenų išrinkimo iš failo algoritmai, pasirinkite bet kurį. Pasinaudoję gautu dvimačiu masyvu (dviejų lygmenų sąrašu) sukurkite
-#naują sąrašą arba žodyną kuriame būtų pateikiama suminė statistika apie kiekvieną žadėją: žaidėjo numeris(kartojasi tik vieną kartą ir suminiai duomenys apie
-#visas rungtynes, taip pat pridėkite papildomą laukelį kuriame nurodomas sužaistų rungtynių skaičius.
+#naują sąrašą arba žodyną kuriame būtų pateikiama suminė statistika (duomenų apie varžybas suma, pvz.: kiek pataikytų tritaškių per visas sužaistas varžybas)
+#apie kiekvieną žadėją: žaidėjo numeris(kartojasi tik vieną kartą ir suminiai duomenys apie
+#visas rungtynes, taip pat pridėkite papildomą laukelį kuriame nurodomas sužaistų rungtynių skaičius (kiek rungtini sužaidė konkretus žaidėjas).
+
+Duomenų išaiškinimas
+23 3 3 3 5 0 0
+23 - Žaidėjo nr
+3 - pataikytų baudinių sk
+3 - mestų baudinių sk
+3 - pataikytų dvitaškių sk
+5 - mestų dvitaškių sk
+0 - pataikytų tritaškių sk
+0 - mestų tritaškių sk
