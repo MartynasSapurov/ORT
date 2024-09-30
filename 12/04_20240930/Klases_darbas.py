6088d895140df320a5986f5c4b0bec2b026319fa
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-09-30 08:14:24 +0300

    Create Klases_darbas.py

---
+++ 12/04_20240930/Klases_darbas.py
@@ -0,0 +1,5 @@
+#1. Parašykite programą kuri nuskaitytų duomenys iš failo "NoahAnimals.txt" ir sudėtų į sąrašą.
+#2. Visus tyruosius Nojaus gyvūnus šiame sąraše pakeiskite didžiojomis raidėmis, o netyruoius mažosiom
+#3. Patobulinkite programą, taip, kad 2 užduotis būtų atliekama atskira funkcija, globaliųjų kintamųjų nenaudokite
+#4. Atsakymą įrašykite į failą "Result.txt" po 6 gyūnus į eilutę atsk
+#5. Patobulinkite programą, taip, kad 4 užduotis būtų atliekama atskira funkcija, globaliųjų kintamųjų nenaudokite
