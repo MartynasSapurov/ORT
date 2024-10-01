83d9ff8f55c2004222db07fcfbc6fd5080b78160
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-01 06:31:11 +0300

    Create Klases_darbas.py

---
+++ 10B/4_pamoka_20241005/Klases_darbas.py
@@ -0,0 +1,22 @@
+
+#1.Akvariumo žuvytė kainuoja x Eur, o sraigė – y Eur. Kokią sumą reikia sumokėti už n sraigių, dukart tiek
+#žuvyčių, dar dvi žuvytes ir dar vieną sraigę? Parašykite programą šiam uždaviniui spręsti.
+#Pasitikrinkite. Kai x = 2, y = 3, n = 10, tai visas pirkinys kainuos 77 Eur.
+
+#2. Lėktuvas, kurio ilgis x metrų, rieda pakilimo taku, kurio plotis lygus trims lėktuvo ilgiams, o ilgis – y kartų
+#didesnis už plotį. Koks pakilimo tako plotas ir koks jį juosiančios tvoros ilgis? Parašykite programą šiam
+#uždaviniui spręsti.
+#Pasitikrinkite. Kai x = 20, y = 3, tai plotas = 10800, o ilgis = 480.
+
+#3. Taupyklėje guli n1 vieno cento vertės monetų, n2 – dviejų centų, n5 – penkių centų ir n10 – dešimties
+#centų vertės monetų. Kokia suma guli taupyklėje? Išreikškite atsakymą eurais ir centais.
+#Pasitikrinkite. Kai n1 = 3, n2 = 8, n5 = 6, n10 = 10, tai taupyklėje yra 1 Eur ir 49 ct.
+
+#4. Vienas centimetras metalinio strypo sveria x gramų. Kiek kilogramų ir gramų sveria m metrų ir c centimetrų
+#tokio strypo?
+#Pasitikrinkite. Kai x = 3, m = 123, c = 60, tai turi būti išvesta 37 kg ir 80 g.
+
+#5. n draugų, susidėję po m centų, už visus pinigus pirko pieštukų, kurių kiekvienas kainuoja p centų, ir juos
+#po lygiai pasidalino. Likusius pinigus ir pieštukus jie atidavė į klasės fondą. Kiek centų ir pieštukų yra
+#klasės fonde?
+#Pasitikrinkite. Kai n = 5, m = 10, p = 3, tai klasės fonde bus 2 centai ir 1 pieštukas
