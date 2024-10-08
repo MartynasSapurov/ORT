6ea9a042fc63a9d8dae8a440d64d0b1c37324d8e
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-08 09:01:06 +0300

    Create Klases_darbas.py

---
+++ 10B/05_Pamoka_20241008/Klases_darbas.py
@@ -0,0 +1,31 @@
+a = int(input("Įveskite pirmąjį skaičių: "))
+b = int(input("Įveskite antrąjį skaičių: "))
+
+"""
+if a == b:
+    result = "Šie skaičiai yra lygūs"
+elif a > b:
+    result = f"{a} yra didesnis už {b}"
+else:
+    result = f"{b} yra didesnis už {a}"
+"""
+"""
+if a == b:
+    result = "Skaičiai yra lygūs"
+else:
+    result = "Skaičiai yra nelygūs"
+
+
+print(result)
+
+result = "Skaičiai yra lygūs" if a == b else "Skaičiai yra nelygūs"
+"""
+"""
+if a == b or a > b:
+    result = "a nemažesnis už b"
+else:
+    result = "a mažesnis už b"
+"""
+print(result)
+
+#Svarankiškai patobulinkite programą, taip, kad būtų tikrinami trys skaičiai ir išvedamas didžiausias
