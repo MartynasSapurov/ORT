278183b9f9fa48ac171da8bf0bae722ac702d1d5
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-14 08:43:12 +0300

    Update Klases_darbas.py

--- 12/06_20241014/Klases_darbas.py
+++ 12/06_20241014/Klases_darbas.py
@@ -2,7 +2,7 @@
 #Atsakymo pavyzdys
 #{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
 
-#2. Taikydami fuknciją filter() iš žodžių sąrašo išrinkite tiktai polindromus (vienodi iš abiejų pusių)
+#2. Taikydami fuknciją filter() iš žodžių sąrašo išrinkite tiktai palindromus (vienodi iš abiejų pusių)
 ["raDar", "leVel", "ciVic", "roBot", "roTor",
 "juMble", "raCecar", "maDam", "noOn", "kaYak",
 "woW", "lighT", "deiFied", "repAper", "brighT",
