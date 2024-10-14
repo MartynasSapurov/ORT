7235515e51de727fab75ea828a942e5e09c544d5
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-14 08:33:26 +0300

    Update Klases_darbas.py

--- 12/06_20241014/Klases_darbas.py
+++ 12/06_20241014/Klases_darbas.py
@@ -1,4 +1,4 @@
-#1. Pasitelkę sąrašų generatorių skurkite žodyną kurio indeksai yra raktai tarp 1 ir 15, o vertės yra jų indeksų kvadratai.
+#1. Pasitelkę sąrašų generatorių sukurkite žodyną kurio indeksai yra raktai tarp 1 ir 15, o vertės yra jų indeksų kvadratai.
 #Atsakymo pavyzdys
 #{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
 
