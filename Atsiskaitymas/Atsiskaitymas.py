7b27f47831974c27bb99c74a3f9394dfaaa10d26
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-14 07:48:13 +0300

    Create Atsiskaitymas.py

---
+++ Atsiskaitymas/Atsiskaitymas.py
@@ -0,0 +1,21 @@
+#1. Sukurkite programą kuri nuskaitytų skaičius iš failo duomenys.txt, surušiuotu juos eilės
+#tvarka ir atsakymą įrašytų į failą rezultatai.txt
+
+#2. Pasitelkię sąrašų generuokite sukurkite sąrašą sudarytą iš 50 atsitiktinių skaičių nuo 1 iki 200
+#pastelkę sąrašų generatorius skaičius nuo 1 iki 100 sukelkite į vieną kortežą (tuple) o likusius į kitą
+#kortežus pavadinkite my_tuple_low ir my_tuple_high
+
+#3. Parašykite funkcija my_func kuri neturi nė vieno parametro, jos tikslas nuksaityti klaviatūra
+#įvestus duomenis pašalinti iš jų skyrybos ženklus ("," "." ":" " " ";" "-"), tada patikrinti
+#gautos teksto eilutės ilgį, jei ilgis siekia bent 5 elementus funkcija grąžiną teksto eilutę,
+#jei ne grąžina teksta "Įvesti netinkami duomenys"
+
+#4. Duotas sąrašas
+["avi", "Noam", "eItan", "Shira", "yaEl", "lioR",
+"Yoni", "tAlia", "Moshe", "Miriam", "eliEzer", "Rivka",
+"sHlomo", "avIva", "daniEl", "yItzhak", "Esther", "Dov",
+"tziPorah", "zohAr", "naoMi", "Levi", "chaiM", "taMar",
+"Rachel", "daviD", "Leah", "shmuEl", "Eliyah", "saRah"]
+#Taikydami for ciklą pakesikite žodį taip kad jis ptasidėtų didžiąja raide,
+#o taikydami "continue" sąlygąpraleiskite žodžius
+#prasidedančius didžiąja raide,
