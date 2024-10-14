0edc7a9f1e41d9f481e5a3a22b3f99db9bdfa573
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-14 08:18:06 +0300

    Update Klases_darbas.py

--- 12/06_20241014/Klases_darbas.py
+++ 12/06_20241014/Klases_darbas.py
@@ -2,4 +2,12 @@
 #Atsakymo pavyzdys
 #{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
 
-#2. Taikydami fuknciją filter() iš žodžių sąrašo input_data = ['Страна', 'шалаш', 'Летел', 'вертолёт', 'УЧУ', 'мэм', 'язык']
+#2. Taikydami fuknciją filter() iš žodžių sąrašo
+["raDar", "leVel", "ciVic", "roBot", "roTor",
+"juMble", "raCecar", "maDam", "noOn", "kaYak",
+"woW", "lighT", "deiFied", "repAper", "brighT",
+"roTator", "stAts", "tiGer", "reFer", "tenEt",
+"malaYalam", "raiNbow", "booK", "hanNah", "evE",
+"cattLe", "redDer", "minIm", "sunSet", "plaNet",
+"yoGurt", "traVel", "kitTen", "proPhet", "magNet",
+"quiCk", "shaDow", "roAd", "appLe", "wiDget"]
