a7520ca177e7edefcc9104778dd1af9dae85db24
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-14 08:12:17 +0300

    Create Klases_darbas.py

---
+++ 12/06_20241014/Klases_darbas.py
@@ -0,0 +1,5 @@
+#1. Pasitelkę sąrašų generatorių skurkite žodyną kurio indekai yra raktai tarp 1 ir 15, o vertės yra jų indeksų kvadratai.
+#Atsakymo pavyzdys
+#{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
+
+#2. Taikydami fuknciją filter() iš žodžių sąrašo input_data = ['Страна', 'шалаш', 'Летел', 'вертолёт', 'УЧУ', 'мэм', 'язык']
