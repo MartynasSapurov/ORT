7b8750891e9060a1d87b3d9b53c6d250624b1e29
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-17 09:47:13 +0300

    Create Klases_darbas.py

---
+++ 10A/05_Pamoka_20241017/Klases_darbas.py
@@ -0,0 +1,8 @@
+#Parašykite programšykite programą kuri paprašytų iš pradžių įvesti jūsų vardą po to amžių.
+#Patikrinkite ar įvestas amžius yra skaičius (age.isdigit())
+#Jeigu ne parašykite "Įvesti netinkami duomenys"
+#tada patikrinkite ar amžius nelygus 0, jeigu lygus parašykite "Įvesti netinkami duomenys"
+#Jei amžius didesnis už 0 ir mažesnis už 10 parašykite "Labas jaunikaiti {Vardas}"
+#Jei amžius didesnis arba lygus 10 ir mažesnis arba lygus 18 parašykite "Kaip sekasi {Vardas}"
+#Jei amžius didesnis už 18 ir mažesnis nei 100 paraškite "Ko peigeidaujate {Vardas}"
+#Visais kiatis atvejais parašykite "{Vardas} jūs meluojate, jums negali būti tiek metų"
