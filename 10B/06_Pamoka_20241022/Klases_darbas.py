e62b68a9e4375072c089ae8014c14ae6b25ffd9a
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-22 08:55:46 +0300

    Create Klases_darbas.py

---
+++ 10B/06_Pamoka_20241022/Klases_darbas.py
@@ -0,0 +1,20 @@
+#Paprasčiausi for ciklai
+#range(start, stop, step)
+x = range(9, 0, 1)
+
+for i in x:
+    print(i)
+#Atspausdinkite n snaigių.
+
+#Įvedimas
+#Natūralus skaičius n (1 ≤ n ≤ 100).
+#Išvestis
+#Išveskite snaiges skirtingose eilutėse.
+#Pasitikrinimui n = 5
+"""
+*
+*
+*
+*
+*
+"""
