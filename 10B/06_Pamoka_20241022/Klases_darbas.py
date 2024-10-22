845f21583861e5b33b78c41e820ac63ea006694a
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-22 09:00:25 +0300

    Update Klases_darbas.py

--- 10B/06_Pamoka_20241022/Klases_darbas.py
+++ 10B/06_Pamoka_20241022/Klases_darbas.py
@@ -4,7 +4,8 @@ x = range(9, 0, 1)
 
 for i in x:
     print(i)
-#Atspausdinkite n snaigių.
+
+#1. Atspausdinkite n snaigių.
 
 #Įvedimas
 #Natūralus skaičius n (1 ≤ n ≤ 100).
@@ -18,3 +19,16 @@ for i in x:
 *
 *
 """
+#2. Marse savaitę sudaro n dienų. Lyginėmis savaitės dienomis sninga, nelyginėmis - šlapdriba. Atspausdinkite savaitės orų tendenciją.
+
+#Įvedimas
+#Natūralus skaičius n (1 ≤ n ≤ 100).
+#Išvestis
+#Jei sninga - **, jei šlapdriba - *0.
+#Pavyzdys
+#n = 5
+#*0
+#**
+#*0
+#**
+#*0
