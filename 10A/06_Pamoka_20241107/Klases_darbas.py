daad7b66e4190870c95611af3022f3509823731f
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-11-07 09:49:12 +0200

    Update Klases_darbas.py

--- 10A/06_Pamoka_20241107/Klases_darbas.py
+++ 10A/06_Pamoka_20241107/Klases_darbas.py
@@ -1,1 +1,37 @@
-05_Pamoka_20241017
+#Paprasčiausi for ciklai
+#range(start, stop, step)
+ribos = range(1, 21)
+
+for i in ribos:
+    if (i % 2) == 0:
+        print(f"Skaičius {i} yra lyginis")
+    else:
+        print(f"Skaičius {i} yra nelyginis")
+
+#1. Atspausdinkite n snaigių.
+
+#Įvedimas
+#Natūralus skaičius n (1 ≤ n ≤ 100).
+#Išvestis
+#Išveskite snaiges skirtingose eilutėse.
+#Pasitikrinimui n = 5
+"""
+*
+*
+*
+*
+*
+"""
+#2. Marse savaitę sudaro n dienų. Lyginėmis savaitės dienomis sninga, nelyginėmis - šlapdriba. Atspausdinkite savaitės orų tendenciją.
+
+#Įvedimas
+#Natūralus skaičius n (1 ≤ n ≤ 100).
+#Išvestis
+#Jei sninga - **, jei šlapdriba - *0.
+#Pavyzdys
+#n = 5
+#*0
+#**
+#*0
+#**
+#*0
