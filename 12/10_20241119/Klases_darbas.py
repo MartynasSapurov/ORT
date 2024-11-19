8b0cdf3c6a09e5a4708cec46db64537fd069cd77
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-11-19 11:15:33 +0200

    Update Klases_darbas.py

--- 12/10_20241119/Klases_darbas.py
+++ 12/10_20241119/Klases_darbas.py
@@ -3,7 +3,7 @@ Sukurkite klasę turinčią šiuos atributus:
 brand, age, cоlor, mark ir weight.
 
 Ir šiuos metodus:
-move, birthday и stop.
+move, birthday ir stop.
 
-Metodai move ir stop Išveda pranešimus «move» ir «stop», o birthday padidina atributo age vertę1.
-Atributus brand, age ir mark būtina nurodyti kuriant obkeltą.
+Metodai move ir stop Išveda pranešimus «move» ir «stop», o birthday padidina atributo age vertę.
+Atributus brand, age ir mark būtina nurodyti kuriant objektą.
