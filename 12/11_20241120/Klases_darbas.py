b487ba09629e419ad23c053ca5e29bea04eac9a1
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-11-20 09:44:29 +0200

    Update Klases_darbas.py

--- 12/11_20241120/Klases_darbas.py
+++ 12/11_20241120/Klases_darbas.py
@@ -3,8 +3,6 @@
 #2. Sukurkite šiai klasei 4 metodus: arklio_galia() - automobilio galia atspausdinama arklio galiomis (1kW - 1.34 Ag)
 #vaziavimas() - nuskaito nuvažiuotų kilometrų skaičių ir juo padidina ridą ir ją atspausdina
 #horn() - parašo "Pypt"
-#add_atribute() 
-#
-#
-#
-#
+#add_atribute() - sukur objektui nauja atributą busena (tvarkinga, nauja, dauzta ....)
+#Sukrkite bent du šios klasės objektus 
+#Sukurkite trečią šios klasės objektą pavdinimu bus ir pridėkitye jam papildomą atributą bus=True
