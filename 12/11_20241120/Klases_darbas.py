c449ac7cd9746921807ff3e4e1da0840a82f24b4
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-11-20 08:19:35 +0200

    Create Klases_darbas.py

---
+++ 12/11_20241120/Klases_darbas.py
@@ -0,0 +1,10 @@
+#1. Sukurkite automobilių klasę kurią sudarys šie atributai: markė, pagaminimo_metai, rida, metai, spalva, galia
+#Kuriant naują objektą būtina nurodyti markę,pagaminimo metus ir ridą
+#2. Sukurkite šiai klasei 4 metodus: arklio_galia() - automobilio galia atspausdinama arklio galiomis (1kW - 1.34 Ag)
+#vaziavimas() - nuskaito nuvažiuotų kilometrų skaičių ir juo padidina ridą ir ją atspausdina
+#horn() - parašo "Pypt"
+#add_atribute() 
+#
+#
+#
+#
