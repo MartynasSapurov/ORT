d9a68d7c61a5a8b7bd8cd3b3d8d29f02e28186cc
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-11-25 09:33:50 +0200

    Update Klases_darbas.py

--- 12/12_20241125/Klases_darbas.py
+++ 12/12_20241125/Klases_darbas.py
@@ -1,3 +1,4 @@
+from random import randint as random
 #Pasitelkę sąrašų generatorių sukurkite 1000 atsitikrinių skaičių nuo 0 iki 99 sąrašą
 my_number_list = [random(0, 99) for i in range(1000)]
 #Sąrašą surušiuokite ir pasitelkę time biblioteką išmatuokite kiek trunka sąrašo surūšiavimas
