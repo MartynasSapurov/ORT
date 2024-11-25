f6505608c6b152e63e07bc545636616b4de241eb
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-11-25 09:30:39 +0200

    Create Klases_darbas.py

---
+++ 12/12_20241125/Klases_darbas.py
@@ -0,0 +1,3 @@
+#Pasitelkę sąrašų generatorių sukurkite 1000 atsitikrinių skaičių nuo 0 iki 99 sąrašą
+my_number_list = [random(0, 99) for i in range(1000)]
+#Pasitelkę time biblioteką išmatuokite kiek trunka sąrašo surūšiavimas
