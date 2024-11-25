7e5116a123d81e41dbb50fa9823eed0046aa9a85
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-11-25 09:49:57 +0200

    Update Klases_darbas.py

--- 12/12_20241125/Klases_darbas.py
+++ 12/12_20241125/Klases_darbas.py
@@ -1,4 +1,12 @@
-from random import randint as random
-#Pasitelkę sąrašų generatorių sukurkite 1000 atsitikrinių skaičių nuo 0 iki 99 sąrašą
-my_number_list = [random(0, 99) for i in range(1000)]
+#Pasitelkę sąrašų generatorių sukurkite 100000 atsitikrinių skaičių nuo 0 iki 99 sąrašą
+my_unsorted_list = [random(0, 99) for i in range(100000)]
+
+my_list_for_sorting = my_unsorted_list
 #Sąrašą surušiuokite ir pasitelkę time biblioteką išmatuokite kiek trunka sąrašo surūšiavimas
+start_time = time()
+my_list_for_sorting.sort()
+time_delta=time() - start_time
+print(time_delta)
+
+#Sukurkit klasę kurioje bus vienas atributas ir vienas metodas, atributas yra tuščias sąrašas į kurį
+#objekto kūrimo metu įrašomas nerūšiuotas sąrašas, o metodas surūšiuoja sąrašą burbulo rušiavimo algoritmu
