007e72209286c344839236dde7f5fb6cdd93a628
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-01-14 06:47:59 +0200

    Update Klases_darbas.py

--- 10B/07_Pamoka_20250114/Klases_darbas.py
+++ 10B/07_Pamoka_20250114/Klases_darbas.py
@@ -2,11 +2,12 @@
 1. Marse savaitę sudaro n dienų. Lyginėmis savaitės dienomis sninga, nelyginėmis - šlapdriba. Atspausdinkite sunumeruotą savaitės orų tendenciją.
 Įvestis:
 Natūralus skaičius n(1 <= n <= 100).
-Išvęstis:
+Išvestis:
 Atspausdinkite savaitės dienos numerį bei numatomą orą. Jei sninga - "**", jei šlapdriba - "*0".
-Pavyzdžiui:
-Jei įvedama 5
-Išvedama
+Pavyzdys:
+Įvedama:
+5
+Išvedama:
 1 *0
 2 **
 3 *0
@@ -14,5 +15,29 @@ Išvedama
 5 *0
 """
 """
-2. 
+2. Atspausdinkite visų skaičių nuo a iki n (imtinai) kvadratus.
+Įvęstis:
+Du sveiki skaičiai a, n
+Išvestis:
+Kvadratines reikšmes išvesti skirtingose eilutėse.
+Pavyzdys:
+Įvedama:
+2 5
+Išvedama:
+4
+9
+16
+25
+"""
+"""
+3. Apskaičiuokite sumą skaičių nuo a iki n (imtinai).
+Įvęstis:
+Du sveiki skaičiai a, n
+Išvestis:
+Išvesti atsakymą.
+Pavyzdys:
+Įvedama:
+2 5
+Išvedama:
+14
 """
