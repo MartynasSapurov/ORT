32d6d9f35255ad8caf7bed75d266dea858db5173
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-01-14 06:40:21 +0200

    Update Klases_darbas.py

--- 10B/07_Pamoka_20250114/Klases_darbas.py
+++ 10B/07_Pamoka_20250114/Klases_darbas.py
@@ -1,1 +1,14 @@
-
+1.
+Marse savaitę sudaro n dienų. Lyginėmis savaitės dienomis sninga, nelyginėmis - šlapdriba. Atspausdinkite sunumeruotą savaitės orų tendenciją.
+Įvestis:
+Natūralus skaičius n(1 <= n <= 100).
+Išvęstis:
+Atspausdinkite savaitės dienos numerį bei numatomą orą. Jei sninga - "**", jei šlapdriba - "*0".
+Pavyzdžiui:
+Jei įvedama 5
+Išvedama
+1 *0
+2 **
+3 *0
+4 **
+5 *0
