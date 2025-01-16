66987f1a1db7860fa4d6824879baa58f7cac4c10
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-01-16 09:37:01 +0200

    Create Klase_darbas.py

---
+++ 10A/07_Pamoka_20250116/Klase_darbas.py
@@ -0,0 +1,46 @@
+a, b = input().split()
+a = int(a)
+b = int(b) 
+"""
+1. Marse savaitę sudaro n dienų. Lyginėmis savaitės dienomis sninga, nelyginėmis - šlapdriba. Atspausdinkite sunumeruotą savaitės orų tendenciją.
+Įvestis:
+Natūralus skaičius n(1 <= n <= 100).
+Išvestis:
+Atspausdinkite savaitės dienos numerį bei numatomą orą. Jei sninga - "**", jei šlapdriba - "*0".
+Pavyzdys:
+Įvedama:
+5
+Išvedama:
+1 *0
+2 **
+3 *0
+4 **
+5 *0
+"""
+"""
+2. Atspausdinkite visų skaičių nuo a iki n (imtinai) kvadratus.
+Įvęstis:
+Du sveiki skaičiai a, n
+Išvestis:
+Kvadratines reikšmes išvesti skirtingose eilutėse.
+Pavyzdys:
+Įvedama:
+2 5
+Išvedama:
+4
+9
+16
+25
+"""
+"""
+3. Apskaičiuokite sumą skaičių nuo a iki n (imtinai).
+Įvęstis:
+Du sveiki skaičiai a, n
+Išvestis:
+Išvesti atsakymą.
+Pavyzdys:
+Įvedama:
+2 5
+Išvedama:
+14
+"""
