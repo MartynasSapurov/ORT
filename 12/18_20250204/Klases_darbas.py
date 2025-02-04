3608a176eb14dc5ae68dd2df4a365bbbac9510d3
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-02-04 10:31:47 +0200

    Create Klases_darbas.py

---
+++ 12/18_20250204/Klases_darbas.py
@@ -0,0 +1,14 @@
+#Duotas failas U1.txt, jame pateikta informacija apie krepšininkus, pirmoje eilutėje nurodoma, kiek yra įrašų, visose
+#kitose pirmas skaičius žaidėjoi numeris, antras skaičius nurodod kiek eilutėje įrašų apie žaidėją, vėliau seka
+#žaistas laikas (teigiami skaičiai), praleistas laikas neigiamis kaičiai
+#1. Perskaitykite iš failo pirmąjį įrašą ir atspausdinkiyte ekrane įrašų skaičių
+
+#2.Nuskaitykite tik zaideju numerius į sąrašą zaidejai ir jį surūšiuokite
+
+#3.Pridėkite papildomus narius (visą eilutę duomenų į sūrūšiuotą sąrašą, tai kad prie žaidėjo numerio atsirastų
+#informaciją apie žaistas minutes
+ 
+#4.patikrinkite visų žaidėjų visus laikus (tik tiek laikų kiek nurodyta antruoju skaičiumi) ir pridėkite prie saraso
+#papildomus du skaicius (žasita ir praleista minučių)
+
+#5. Atspausdinkite, tik žaidėjo numerį viso žaista ir viso praleistą iš sukurto sątašo į failą rez1.txt
