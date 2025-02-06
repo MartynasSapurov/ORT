55d0a50e4dc04347649883709cccfcbfcc4d4232
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-02-06 10:13:09 +0200

    Update Klases_darbas.py

--- 10A/08_Pamoka_20250206/Klases_darbas.py
+++ 10A/08_Pamoka_20250206/Klases_darbas.py
@@ -1,7 +1,13 @@
 #10 kartų nuskaitykite klaviatūra įvedamą sveikąjį skaičių, atsakyme patskirai pateikite lyginių ir nelyginių skaičių sumas
+
 suma_lyginiu=0
 suma_nelyginiu=0
-suma = 0
 
 for _ in range(10):
-    suma += int(input("Įveskite sveikąjį skaičių\n"))
+    skaicius=int(input("Įveskite sveikąjį skaičių\n"))
+    if skaicius % 2:
+        suma_nelyginiu += skaicius
+    else:
+        suma_lyginiu += skaicius
+
+print(f"Lyginių skaičių suma {suma_lyginiu}, o nelyginių skaičių suma {suma_nelyginiu}")
