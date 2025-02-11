52dab3f5d0c242cb8bdfd11bc1171e0e24ce5332
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-02-11 10:16:24 +0200

    Create Klasės_darbas.py

---
+++ 12/19_20250211/Klasės_darbas.py
@@ -0,0 +1,6 @@
+#Sukurkite programą, kuri palygintų quick ir buble sort algoritmų veikimo trukmę įvairiais atvejais:
+#Kai generuojas sąraųas iš 100 atsitiktinių skaičių
+#Kai 100 atsitiktinių skaičių pateikiama priešinga eilės tvarka
+#Kai pusė sąrašo yra surūšiuota pusė ne
+#Sugalvokite papildom1 atvejį savo nuožiūra
+#Rezultatus įrašykite į failą Rezult.txt
