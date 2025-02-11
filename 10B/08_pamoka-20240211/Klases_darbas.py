55fdd906fc79bc836c90262e1f4c562d1a9e910c
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-02-11 09:16:29 +0200

    Update Klases_darbas.py

--- 10B/08_pamoka-20240211/Klases_darbas.py
+++ 10B/08_pamoka-20240211/Klases_darbas.py
@@ -5,5 +5,9 @@ suma_nelyginiu=0
 
 for _ in range(10):
     skaicius=int(input("Įveskite sveikąjį skaičių\n"))
+    if skaicius % 2:
+        suma_nelyginiu += skaicius
+    else:
+        suma_lyginiu += skaicius
 
 print(f"Lyginių skaičių suma {suma_lyginiu}, o nelyginių skaičių suma {suma_nelyginiu}")
