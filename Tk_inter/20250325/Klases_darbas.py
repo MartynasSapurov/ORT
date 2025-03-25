917ae65546fd2fcf43fdbe78407ebb8aef80225e
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-03-25 15:19:44 +0200

    Update Klases_darbas.py

--- Tk_inter/20250325/Klases_darbas.py
+++ Tk_inter/20250325/Klases_darbas.py
@@ -1,1 +1,12 @@
+import tkinter as tk
+window = tk.Tk()
+label = tk.Label(window, text="Labas pasauli!", bg="blue")
 
+button = tk.Button(window,
+    text="Paspausk mane",
+    width=25,
+    height=5
+)
+label.pack()
+button.pack()
+window.mainloop()
