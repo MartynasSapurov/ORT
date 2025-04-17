e8842925f06ad9a2f83130f0c068fd9d056fe117
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-04-17 10:02:23 +0300

    Update Pagalba.py

--- 10B/11_Pamoka_20250401/Pagalba.py
+++ 10B/11_Pamoka_20250401/Pagalba.py
@@ -4,3 +4,20 @@ kaina = pradine_kaina
 for i in range(zuvu_kiekis):
     print(kaina)
     kaina=kaina*pradine_kaina
+
+pradine_kaina = int(input())
+zuvu_kiekis = int(input())
+kaina = pradine_kaina
+for i in range(zuvu_kiekis):
+    print(kaina)
+    kaina=kaina*pradine_kaina
+
+pirminis = False
+
+if n % i:
+    pirminis = True
+    
+if pirminis == True:
+    print("TAIP")
+else:
+    print("NE")
