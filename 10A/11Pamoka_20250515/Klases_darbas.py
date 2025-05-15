201b2aa05db42c664e202f03766fb552edc06b18
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-05-15 09:54:06 +0300

    Create Klases_darbas.py

---
+++ 10A/11Pamoka_20250515/Klases_darbas.py
@@ -0,0 +1,40 @@
+"""
+faktorial = 1
+for i in range(1, 10):
+    faktorial*=i
+print(faktorial)
+"""
+"""
+i = 5
+
+while i<=6:
+    print(i)
+    if i == 0:
+        break
+    i-=1
+"""
+"""
+n = 10
+i = 0
+suma = 0
+
+while i <=10:
+    i += 1
+    if i %2:
+        continue
+    suma+=i
+    print(i)
+
+print(suma)
+"""
+"""
+i = 0
+
+while i < 5:
+    i+=1
+    print(i)
+    if i==6:
+        break
+else:
+    print(f"{i} nebėra mažiau už 6")
+"""
