e8e10f1cd50fd0d8b8397857c59548e2e290b0a5
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-09-09 11:29:48 +0300

    Create Klases_darbas.py

---
+++ 11_2025_2026/1_pamoka/Klases_darbas.py
@@ -0,0 +1,49 @@
+"""
+a = 9
+b = 7.5
+c = True
+d = ""
+e = None
+f = 'Martynas'
+
+print(a, end=" ")
+print(type(a), id(a))
+
+print(b, end=" ")
+print(type(b), id(b))
+
+print(c, end=" ")
+print(type(c), id(c))
+
+print(d, end=" ")
+print(type(d), id(d))
+
+print(e, end=" ")
+print(type(e), id(e))
+
+print(f, end=" ")
+print(type(f), id(f))
+"""
+
+"""
+a = 5
+
+print(a, id(a))
+a=6
+print(a, id(a))
+
+b=True
+print(b, id(b))
+
+b=False
+print(b, id(b))
+"""
+
+a = input("Įveskite skaičių")
+
+a.
+if a.isdigit():
+    a=int(a)
+    print(a, type(a))
+else:
+    print("Įvestas ne skaičius")
