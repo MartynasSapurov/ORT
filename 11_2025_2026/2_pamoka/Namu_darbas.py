9fe58b97f40a07444fee02ef696df7e5b4c30150
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-09-15 17:48:24 +0300

    Update Namu_darbas.py

--- 11_2025_2026/2_pamoka/Namu_darbas.py
+++ 11_2025_2026/2_pamoka/Namu_darbas.py
@@ -1,3 +1,7 @@
-"""
-Namų darbas
-"""
+#1 Užd
+#Parašykite programą, kuri suskaičiuotų skaičių nuo m iki n kvadratų sumą s = m2 + (m + 1)2 + (m + 2)2 ...+ n2.
+#Pasitikrinkite. Kai m = 3, n = 6, tai atsakymas: 86
+
+#Parašykite programą, kuri suskaičiuotų, kiek yra keturženklių skaičių, kurių pirmųjų dviejų skaitmenų suma yra
+#lygi likusių dviejų skaitmenų sumai, ir išvestų juos į ekraną.
+#Pasitikrinkite. Atsakymas turi būti: 615
