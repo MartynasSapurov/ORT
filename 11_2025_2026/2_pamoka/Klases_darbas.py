8937e6b6a3b85fcb4783bec41f981454e28a72ef
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-09-15 09:25:30 +0300

    Create Klases_darbas.py

---
+++ 11_2025_2026/2_pamoka/Klases_darbas.py
@@ -0,0 +1,64 @@
+my_list = ["Bernardas", "Emil", "Austėja", "Daniel", "Daniel", "Natan", "Maksim", "Augustas", "Benas", "Kornėjus"]
+my_tuple = ("Bernardas", "Emil", "Austėja", "Daniel", "Daniel", "Natan", "Maksim", "Augustas", "Benas", "Kornėjus")
+my_set = {"Bernardas", "Emil", "Austėja", "Daniel", "Daniel", "Natan", "Maksim", "Augustas", "Benas", "Kornėjus"}
+my_dict = {"dog": "šuo", "cat":"katė", "Peter":"Griffin"}
+
+"""
+for item in my_list:
+    print(item)
+"""
+"""
+for i in range(0, len(my_list)):
+    print(my_list[i])
+"""
+
+"""
+print(type(my_list))
+print(type(my_tuple))
+
+my_list.append("Martynas")
+print(my_list)
+"""
+
+"""
+add_tuple=("Martynas", "Saulius")
+my_tuple=my_tuple+add_tuple
+"""
+"""
+my_tuple=list(my_tuple)
+my_tuple.append("Martynas")
+my_tuple = tuple(my_tuple)
+
+my_list.
+print(my_tuple)
+"""
+
+"""
+my_set = set(my_list)
+
+for item in my_set:
+    print(item)
+
+print("*"*40)
+
+for item in my_list:
+    print(item)
+
+"""
+"""
+print(my_set)
+"""
+"""
+
+for item in my_dict.values():
+    print(item)
+"""
+
+for item, value in my_dict.items():
+    print(item, end=" ")
+    print(value)
+
+    
+"""
+print(my_dict["Peter"])
+"""
