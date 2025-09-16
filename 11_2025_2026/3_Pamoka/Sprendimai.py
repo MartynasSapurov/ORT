1e807d0a6f4fa4ca389abb2bc0d705e21be93eb2
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-09-16 11:54:49 +0300

    Update Sprendimai.py

--- 11_2025_2026/3_Pamoka/Sprendimai.py
+++ 11_2025_2026/3_Pamoka/Sprendimai.py
@@ -1,3 +1,78 @@
 """
 1 Užduotis
+
+
+kiekis = int(input("Įveskite sekos ilgį\n"))
+seka = []
+
+for _ in range(kiekis):
+    seka.append(int(input("Įveskite skaičių ")))
+
+print(seka[0])
+"""
+
+"""
+2 Užduotis
+
+kiekis = int(input("Įveskite sekos ilgį\n"))
+seka = []
+
+for _ in range(kiekis):
+    seka.append(int(input("Įveskite skaičių ")))
+
+print(len(seka))
+print(seka[0]+seka[-1])
+"""
+
+"""
+3 Užduotis
+
+kiekis = int(input("Įveskite sekos ilgį\n"))
+seka = []
+
+for _ in range(kiekis):
+    seka.append(int(input("Įveskite skaičių ")))
+suma = 0
+
+for item in seka:
+    suma += item
+
+#for _ in range(kiekis):
+#    suma += seka[_]
+
+
+
+print(suma)
+"""
+
+"""
+4 Užduotis
+
+kiekis = int(input("Įveskite sekos ilgį\n"))
+seka = []
+
+for _ in range(kiekis):
+    seka.append(int(input("Įveskite skaičių ")))
+
+suma = 0
+
+for item in seka:
+    suma += item
+
+print(suma/kiekis)
+
+"""
+"""
+5 Užduotis
+
+
+kiekis = int(input("Įveskite sekos ilgį\n"))
+seka = []
+
+for _ in range(kiekis):
+    seka.append(int(input("Įveskite skaičių ")))
+
+for i in range(1, kiekis-1):
+    average= (seka[i-1]+seka[i]+seka[i+1])/3
+    print(average)
 """
