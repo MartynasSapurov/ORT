e760f9534ffeaa0ec580b29808b0b4461e4f6dcf
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-13 17:17:23 +0300

    Update Namu_darbas.py

--- 11_2025_2026/5_Pamoka/Namu_darbas.py
+++ 11_2025_2026/5_Pamoka/Namu_darbas.py
@@ -1,1 +1,9 @@
+#1 Užduotis
+#Parašykite programą input() ir print() funkcijoms. Programa prašo įvesti eilutę (string).
+#Po to sukuriami du eilutės (str) tipo kintamieji į vieną jų įrašomi tik nelyginiai įvestos eilutės elementai, į kitą eilutės elementai atvirkštine eilės tvarka.
+#Visi abiejų kintamųjų elementai turi būti užrašomi tik didžiosiomis raidėmis
+#Abu kintamuosius atspausdinkite iš naujos eilutės
 
+#2 Užduotis
+#Pasitelkę sąrašų generatorių sukukite sąrašą su 25 atsitiktiniais skaičiai nuo 1 iki 99 imtinai
+#Pasitelkę sąrašų generatorių sukurkite naują sąrašą į kurį įtrauktumėte tik tuos narius kurių kaimynininių nartių suma > 65 
