505eebfb76f49b2a63fee87decf1bed9a4865a11
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-14 10:44:53 +0300

    Create Klases_darbas.py

---
+++ 11_2025_2026/6_Pamoka/Klases_darbas.py
@@ -0,0 +1,6 @@
+#1 Užduotis
+#Sukurkite tuščią sąrašą my_list
+#Klaviatūra įveskite 10 vardų ne trumpesnių nei 5 simboliai atskirtu tarpais ir įrašykite į sąrašą pasitelkę split metodą
+#Pasitelkę sąrašų generatorių sukurkite naują sąrašą my_new_list į kurį įtraukite visus įrašus iš my_list, net tik simbolius nuo 3 iki paskutinio.  
+#Visui įrašai pateikiami didžiosiomis raidėmis
+#Rezultatą atspausdinkite
