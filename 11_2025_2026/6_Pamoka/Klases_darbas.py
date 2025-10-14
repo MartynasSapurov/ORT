4943080487bdce9ab518b335ea49535183f1ecb1
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-14 10:51:23 +0300

    Update Klases_darbas.py

--- 11_2025_2026/6_Pamoka/Klases_darbas.py
+++ 11_2025_2026/6_Pamoka/Klases_darbas.py
@@ -1,7 +1,7 @@
 #1 Užduotis
 #Sukurkite tuščią sąrašą my_list
 #Klaviatūra įveskite 10 vardų ne trumpesnių nei 5 simboliai atskirtu tarpais ir įrašykite į sąrašą pasitelkę split metodą
-#Pasitelkę sąrašų generatorių sukurkite naują sąrašą my_new_list į kurį įtraukite visus įrašus iš my_list, net tik simbolius nuo 3 iki paskutinio.  
+#Pasitelkę sąrašų generatorių sukurkite naują sąrašą my_new_list į kurį įtraukite visus įrašus iš my_list, bet tik simbolius nuo 3 iki paskutinio.  
 #Visui įrašai pateikiami didžiosiomis raidėmis
 #Rezultatą atspausdinkite
 my_string = "Eagle Sunglasse"
