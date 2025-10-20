077c8378de7c1740a22daeb525e79c71a47cfd5f
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-20 09:29:22 +0300

    Create Klases_darbas.py

---
+++ 11_2025_2026/8_Pamoka/Klases_darbas.py
@@ -0,0 +1,33 @@
+#Duotas gyvūnų sąrašas
+biblical_animals = [
+    "Aquila',
+    "Behemoth",
+    "Cattle",
+    "Camel",
+    "Ewe",
+    "Eagle",
+    "Ferret",
+    "Giraffe",
+    "Horse",
+    "Lion",
+    "Ox",
+    "Pig",
+    "Rabbi",
+    "Ram",
+    "Serpent",
+    "Tiger",
+    "Viper",
+    "Wolf",
+    "Zebra",
+    "Unicorn"  # Note: Unicorn is referenced symbolically in biblical texts
+]
+#Duotas atskiras tyrųjų gyvūnų sąrašas
+pure_biblical_animals = [
+    "Cattle",
+    "Camel",
+    "Ewe",
+    "Eagle",
+    "Ox",
+    "Ram"
+]
+#Patikrinkite ar gyvūnas sąraše biblical_animals yra tyras, jei taip pakeiskite visas raides į didžiąsias ir užrašykite atbulai
