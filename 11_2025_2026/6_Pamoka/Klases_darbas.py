ba1d11ab5f0d7dd62c5b351815d10e92a7b2309a
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-20 09:09:05 +0300

    Update Klases_darbas.py

--- 11_2025_2026/6_Pamoka/Klases_darbas.py
+++ 11_2025_2026/6_Pamoka/Klases_darbas.py
@@ -1,1 +1,14 @@
-sda
+#1 Užduotis
+#Sukurkite tuščią sąrašą my_list
+#Klaviatūra įveskite 10 vardų ne trumpesnių nei 5 simboliai atskirtų tarpais ir įrašykite į sąrašą pasitelkę split metodą
+#Pasitelkę sąrašų generatorių sukurkite naują sąrašą my_new_list į kurį įtraukite visus įrašus iš my_list, bet tik simbolius nuo 3 iki paskutinio.
+#Visui įrašai pateikiami didžiosiomis raidėmis
+#Rezultatą atspausdinkite
+
+#2 Užduotis
+#Pasitelkę sąrašų generatorių sukurkite 20 atsitiktinių skaičių nuo 1 iki 9 sąrašąpavadinimu my_number_list
+#Pasitelkę sąrašų generatorių atnaujinkite sąrašą pakeitę visus narius į jų kvadratą
+
+#3 Užduotis
+#Pasitelkę sąrašų generatorių sukurkite 10 narių žodyną kur indeskai būtų skaičiai nuo 0  iki 10, o vertės atsitiktiniai skaičiai tarp 1 ir 99
+#Pasitelkę sąrašų generatorių tas žodyno vertes kurios mažesnės nei 20 padvigubinkite likusių nekeskite
