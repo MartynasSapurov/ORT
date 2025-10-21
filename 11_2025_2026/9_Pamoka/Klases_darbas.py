99ceb600799187d545b80dc0c6111fdbc57dd1a3
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-21 11:43:01 +0300

    Create Klases_darbas.py

---
+++ 11_2025_2026/9_Pamoka/Klases_darbas.py
@@ -0,0 +1,24 @@
+#File open
+file = open('failas_Kajaus.txt', 'w')
+file.write("Labas\nKą tu dabar darai?\nIki")
+file.close()
+
+print(40*"*")
+"""
+with open('failas_Kajaus.txt', 'r') as file:
+    print(file.readline(), end='')
+    print(file.readline(), end='')
+    print(file.readline(), end='')
+"""
+with open('failas_Kajaus.txt', 'r') as file:
+    print(file.readlines())
+
+print(40*"*")
+#Try file open konstruktorius
+try:
+    file = open('failas_Kajaus.txt')
+    a = file.read()
+    print(a)
+    print(type(a))
+finally:
+    file.close()
