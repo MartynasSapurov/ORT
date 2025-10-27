809d147c1bee477fdb5ee914349da13dba9c472d
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-27 10:05:29 +0200

    Update Namu_darbas.py

--- 11_2025_2026/10_Pamoka/Namu_darbas.py
+++ 11_2025_2026/10_Pamoka/Namu_darbas.py
@@ -1,1 +1,13 @@
+#Duota simbolių eilutė, kiekvienas atlikte visu variantus:
 
+# b'\xc5\xa0ven\xc4\x8dion\xc4\x97li\xc5\xb3'
+# b'\xc5\xa0e\xc5\xa1iasde\xc5\xa1imt\xc4\x85j\xc4\xaf'
+# b'D\xc5\xabk\xc5\xa1t\xc5\xb3 \xc4\x85\xc5\xbeuolynas'
+# b'\xc5\xbdvirg\xc5\xbed\xc5\xb3 e\xc5\xbeeras'
+# b'S\xc4\x85\xc5\xa1lav\xc5\xb3 s\xc4\x85\xc5\xa1lavynas'
+# b'\xc5\xbdie\xc5\xbeirb\xc5\xb3 s\xc4\x85ranka'
+
+#Iškoduokite šią eilute taikydami numatytąją kodavimo sistemą (codec)
+#Tuomet gautą rezultatą užkoduokite taikydami Latin4 kodavimo sistemą (codec)
+#Palyginkite pradinės Utf-8 užkoduotos simbolių eilutės ilgį su naujai užkoduotos simbolių eilutės ilgiu
+#Visus rezultatus atspausdinkite
