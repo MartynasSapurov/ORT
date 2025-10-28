cdc9d207dd6b3fee331f493d5064540b0b2f2cd2
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-10-28 11:09:09 +0200

    Update Klases_darbas.py

--- 11_2025_2026/11_Pamoka/Klases_darbas.py
+++ 11_2025_2026/11_Pamoka/Klases_darbas.py
@@ -8,7 +8,7 @@ e = b'S\xc4\x85\xc5\xa1lav\xc5\xb3 s\xc4\x85\xc5\xa1lavynas'
 f = b'\xc5\xbdie\xc5\xbeirb\xc5\xb3 s\xc4\x85ranka'
 
 utf = [a, b, c, d, e, f]
-str_ = []
+#str_ = []
 
 #Iškoduokite šią eilute taikydami numatytąją kodavimo sistemą (codec)
 #Tuomet gautą rezultatą užkoduokite taikydami Latin4 kodavimo sistemą (codec)
@@ -25,3 +25,7 @@ print(a_latin, len(a_latin))
 """
 
 #Taikydami sąrašų generatorių užpildykite sąrašą str
+print(utf)
+str_ = [item.decode() for item in utf]
+print(str_)
+#Taikydami sąrašų generatorių užpildykite žodyną, latin_dict, kuriame raktas yra kodo ilgis, o vertė kodas Latin4
