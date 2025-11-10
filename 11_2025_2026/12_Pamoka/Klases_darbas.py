831aa8376e0c74e37940f316ac43754fa1e68f93
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-11-10 09:14:45 +0200

    Create Klases_darbas.py

---
+++ 11_2025_2026/12_Pamoka/Klases_darbas.py
@@ -0,0 +1,16 @@
+import csv #Importuojame csv biblioteką
+
+name_of_fields = ["Vardas", "Pavardė", "Gimimo data"]
+duomenys  = [["Kajus",  "Kilšauskas", "2007-10-23"], ["Daniel", "Gustaitis", "2008-05-20"], ["Emil", "Čeliadin", "2008-04-16"],
+             ["Maksim", "Rudek", "2008-03-26"], ["Kornėjus", "Pozdniakovas", "2008-04-05"]]
+
+for item in duomenys:
+    print(item)
+
+#Duomenų įrašymas CSV formatu
+with open("data.csv", mode='w', encoding='utf8', newline='') as file: #Atsidarome failą rašymui, pakeičiame nustatymus,
+# kad nebūtų perkialiama nauja eilutė newline=''
+    file_writer = csv.writer(file) #Susikuriame csv failo rašymo konstruktorių
+    file_writer.writerow(name_of_fields) #Įrašome antraštinę eilutę
+    for item in duomenys:
+        file_writer.writerow(item) #Įrašome pirmają duomenų eilutę
