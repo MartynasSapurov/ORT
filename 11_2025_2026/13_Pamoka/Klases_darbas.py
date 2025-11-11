bd4bbbbf6609f14657384664d96f27ba38ccf504
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-11-11 10:30:33 +0200

    Update Klases_darbas.py

--- 11_2025_2026/13_Pamoka/Klases_darbas.py
+++ 11_2025_2026/13_Pamoka/Klases_darbas.py
@@ -1,2 +1,26 @@
 #1 Nuskaitykite duomeniš failo Animals.csv
-#
+#Jeigu gyvūnas yra tyras pakewiskite Amount į 17, jeigu ne 2
+#Atsakymą įrašykite į failą Answer_1.csv
+
+#2 Nuskaitykite duomeniš failų Safardi.txt ir Ashkenazi.txt iškoduokite simbolius ir
+#abiejų sąrašų duomenis irašykite į failą Rabbi.txt
+
+#3 Duotas Holokausto lietuvoje vietovardsąrašas
+vietoves = [
+    (b'Paneriai (Vilnius)', 70000),
+    (b'Kauno Devintas fortas', 50000),
+    (b'Kauno VII fortas', 3000),
+    (b'Kauno geto likvidavimas', 10000), 
+    (b'\xc5\xa0iauli\xc5\xb3 geto likvidavimas', 8000),
+    (b'K\xc4\x97dainiai', 2000),
+    (b'Plung\xc4\x97', 1800),
+    (b'Tel\xc5\xa1iai / Raini\xc5\xb3 mi\xc5\xa1kas', 1500),
+    (b'Garg\xc5\xbedai', 200),
+    (b'Utena', 2000),
+    (b'Alytus', 1500),
+    (b'\xc5\xa0ven\xc4\x8dionys / \xc5\xa0ven\xc4\x8dion\xc4\x97liai', 3800),
+    (b'Panev\xc4\x97\xc5\xbeys', 8000),
+    (b'Marijampol\xc4\x97', 3800), 
+    (b'Jonava', 500)
+]
+#sukurkite naują sąrašą į kurį įtraukti tik vietovardžiai su lietuviškais rašmenimis liet_vietoves
