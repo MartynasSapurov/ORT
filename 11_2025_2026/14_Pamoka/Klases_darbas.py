1224206866a510f2a89930a88b5a3b4611e45269
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-11-17 09:15:33 +0200

    Update Klases_darbas.py

--- 11_2025_2026/14_Pamoka/Klases_darbas.py
+++ 11_2025_2026/14_Pamoka/Klases_darbas.py
@@ -1,3 +1,4 @@
+"""
 def maksim():
     print("Maksim")
 
@@ -21,3 +22,26 @@ print(dalyba(sk1, y = sk2))
 
 #Parašykite funkcija, kuri nuskaitytų skaičių įvesta klaviatūra ir jeigu tai skaačius atspausdintų "Number",
 #jeigu ne, "Not number", funkcijos pavadinimas tikrinimas()
+"""
+"""
+def tikrinimas():
+
+    if input().isdigit():
+        return "Number"
+    else:
+        return "Not nuber"
+
+print(tikrinimas())
+"""
+
+def dviem_daugiau(x, y):
+    x+=2
+    y+=2
+    return x, y
+
+a, b = dviem_daugiau(1, 2)
+print(a, b)
+
+#Parašykite programą kuri prašytų jūsų įvesti skaičių tol kol bus įvestas skaitmuo, jeigu įvedamas tekstas
+#prašo įvesti skaičių dar kartą, naudokite vieną funkciją skaičiaus įvcedimui ir tikrinimui
+
