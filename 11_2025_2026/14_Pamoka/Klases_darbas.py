277677387d5d58bd135fb45b069ec8e2948c2242
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-11-17 09:00:41 +0200

    Create Klases_darbas.py

---
+++ 11_2025_2026/14_Pamoka/Klases_darbas.py
@@ -0,0 +1,23 @@
+def maksim():
+    print("Maksim")
+
+def kvadratas(x):
+    return x*x
+
+def sandauga(x, y=3):
+    return x*y
+
+def dalyba(x, y):
+    return x/y
+
+sk1 = int(input())
+sk2 = int(input())
+
+print(kvadratas(sk1))
+
+print(sandauga(sk1, sk2))
+
+print(dalyba(sk1, y = sk2))
+
+#Parašykite funkcija, kuri nuskaitytų skaičių įvesta klaviatūra ir jeigu tai skaačius atspausdintų "Number",
+#jeigu ne, "Not number", funkcijos pavadinimas tikrinimas()
