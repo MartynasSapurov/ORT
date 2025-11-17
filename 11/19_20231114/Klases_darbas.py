9a4ef9b63ea87dc520733482a0e5be4e0211f7ec
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-11-17 09:17:08 +0200

    Update Klases_darbas.py

--- 11/19_20231114/Klases_darbas.py
+++ 11/19_20231114/Klases_darbas.py
@@ -1,3 +1,7 @@
+#File open
+file = open('test.txt', 'w')
+file.write('Line 1\nline 2\nline 3')
+file.close()
 """
 def maksim():
     print("Maksim")
@@ -5,15 +9,29 @@ def maksim():
 def kvadratas(x):
     return x*x
 
+print("*"*40)
+#With file open konstruktorius
 def sandauga(x, y=3):
     return x*y
 
+with open('test.txt', 'rt') as file:
+    print(file.readline(1))
 def dalyba(x, y):
     return x/y
 
+print("*"*40)
 sk1 = int(input())
 sk2 = int(input())
 
+#Try file open konstruktorius
+try:
+    file = open('test.txt')
+    a = file.read()
+    print(a)
+    print(type(a))
+    print(a[7:12])
+finally:
+    file.close()
 print(kvadratas(sk1))
 
 print(sandauga(sk1, sk2))
@@ -44,5 +62,4 @@ print(a, b)
 
 #Parašykite programą kuri prašytų jūsų įvesti skaičių tol kol bus įvestas skaitmuo, jeigu įvedamas tekstas
 #prašo įvesti skaičių dar kartą, naudokite vieną funkciją skaičiaus įvcedimui ir tikrinimui
-
 
