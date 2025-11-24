78916ae48a0abebfb7f9de7fed0d070b2b067c6f
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-11-24 08:56:08 +0200

    Create Klases_darbas.py

---
+++ 11_2025_2026/15_Pamoka/Klases_darbas.py
@@ -0,0 +1,26 @@
+"""
+def smile (a):
+    print(a*"\U0001F600")
+"""
+"""
+def dobilas (a):
+    print("\U0001F600", end="")
+    print(a*"\U0001F340", end="")
+    print("\U0001F600")
+"""
+def dobilas (a):
+    print("\U0001F600", end="")
+    for i in range(a):
+        print("\U0001F340", end="")
+    print("\U0001F600")
+
+def smile (a):
+    for i in range(a):
+        print("\U0001F600", end="")
+    print()
+
+
+smile(9)
+for _ in range(5):
+    dobilas(7)
+smile(9)
