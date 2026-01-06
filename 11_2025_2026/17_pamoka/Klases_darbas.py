b3d025a7fbab37c60e89ecf73efe1c2ca000545f
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-01-06 11:43:45 +0200

    Create Klases_darbas.py

---
+++ 11_2025_2026/17_pamoka/Klases_darbas.py
@@ -0,0 +1,32 @@
+"""
+my_list = [5.012345, 1125.125460, 45.458596]
+tinkslumas = [1, 1, 1]
+print(my_list)
+
+print(list(map(round, my_list, tinkslumas)))
+"""
+
+def over_75(x):
+    return x > 75
+
+scores = [66, 90, 68, 59, 76, 60, 88, 74, 81, 65, 75, 99, 12]
+
+#print(list(filter(over_75, scores)))
+"""
+a = int(input())
+b = over_75(a)
+
+print(b)
+
+"""
+print(list(filter(lambda x: x > 75, scores)))
+
+c = lambda x: x > 75
+
+print(c(85))
+
+"""
+Taikydami lambda funkcija i6rinkite i6 s1ra60 my_list tik simetriškus skaišius
+"""
+
+my_list = [121, 122, 12321, 15551, 174, 5885, 456]
