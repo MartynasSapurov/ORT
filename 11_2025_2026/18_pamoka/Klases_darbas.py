56adf1955218ec546dbab4ba7a984cc58f5abdd1
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-01-12 08:46:31 +0200

    Create Klases_darbas.py

---
+++ 11_2025_2026/18_pamoka/Klases_darbas.py
@@ -0,0 +1,3 @@
+#Duotas sąrašas:
+my_list = ["Daniel", "Kornėjus", "Augustas", "Maksim", "Natan", "Kajus", "dAniel", "danielius", "Austėja", "Marta", "Bernardas", "Emil"]
+#Taikydami filter ir lambda funkcijas parašykite programną, kuri išrinktu tik moksleivius kurių vardai prasideda raide "D"
