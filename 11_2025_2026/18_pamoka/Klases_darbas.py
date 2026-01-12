7ca6d440bb043145a214b41d332b01cc1b61c2e6
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-01-12 08:55:59 +0200

    Update Klases_darbas.py

--- 11_2025_2026/18_pamoka/Klases_darbas.py
+++ 11_2025_2026/18_pamoka/Klases_darbas.py
@@ -1,3 +1,17 @@
+#1
 #Duotas sąrašas:
 my_list = ["Daniel", "Kornėjus", "Augustas", "Maksim", "Natan", "Kajus", "dAniel", "danielius", "Austėja", "Marta", "Bernardas", "Emil"]
 #Taikydami filter ir lambda funkcijas parašykite programną, kuri išrinktu tik moksleivius kurių vardai prasideda raide "D"
+
+#2
+#Duoti trys skaičių sąrašai
+nums1 = [1, 2, 3]
+nums2 = [4, 5, 6]
+nums3 = [7, 8, 9]
+
+print("Original list:")
+print(nums1)
+print(nums2)
+print(nums3)
+
+#Raikydami map funkciją parašykite programą kuri sukurtų jnaują sąrašą, kurio elemtai būtų lygųs visų trijų sąršų elemntų sumai
