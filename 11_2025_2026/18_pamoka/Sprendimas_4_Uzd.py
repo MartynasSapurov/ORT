8edc0bf7ffe2f6747b8d0c16b6412f377c83228d
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-01-19 09:40:20 +0200

    Update Sprendimas_4_Uzd.py

--- 11_2025_2026/18_pamoka/Sprendimas_4_Uzd.py
+++ 11_2025_2026/18_pamoka/Sprendimas_4_Uzd.py
@@ -1,3 +1,7 @@
+eluciu_ilgiai = []
+ilgiausia_eiute = 0
+didziausias_ilgis = 0
+
 with open('Data.txt', 'r') as file:
     data = file.read().split("\n")
 
@@ -6,4 +10,22 @@ for i in range(len(data)):
     for item in data[i]:
         if item.isalpha():
             raides_eilueje+=1
-    print(i+1, raides_eilueje)
+    eluciu_ilgiai.append(raides_eilueje) # įrašome kiekvienos eilutes ilgį į sąrašą
+
+
+"""
+for i in range(len(eluciu_ilgiai)):
+    if eluciu_ilgiai[i] > ilgiausia_eilute[1]:
+        ilgiausia_eilute[1] = eluciu_ilgiai[i]
+        ilgiausia_eilute[0] = i
+"""
+didziausias_ilgis = max(eluciu_ilgiai)
+
+for i in range(len(eluciu_ilgiai)):
+    if eluciu_ilgiai[i] == didziausias_ilgis:
+        ilgiausia_eiute = i
+
+print(ilgiausia_eiute+1)
+
+
+
