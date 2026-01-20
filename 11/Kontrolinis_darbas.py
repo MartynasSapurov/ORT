fbf8e3a2561ce114b852fe8ce39c60ba7e563e56
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-01-20 09:55:23 +0200

    Update Kontrolinis_darbas.py

--- 11/Kontrolinis_darbas.py
+++ 11/Kontrolinis_darbas.py
@@ -1,1 +1,42 @@
+#1 Duotas sąrašas 
+[2, 11, 25, 13, 55, 17, 45, 85, 45, 128, 85]
+#Taikydami filter ir lambda funkcijas sukurkite naują sąrašą į kurį patektų til lyginiai skaičiai
 
+#2 Duotas sąrašas
+[2, 11, 25, 13, 55, 17, 45, 85, 45, 128, 85]
+#Taikydami map if lambda funkcijas sukurkite naują sąrašą kurį sudarytų duotų skaičių vertės padauginos iš 6
+
+#3 Duoti du sąrašai
+[8, 4 , 11, 15, 52, 45, 12]
+
+[1, 17, 25, 45, 11, 12, 13]
+#Taikydami map funkciją sukurkite nauiją sąrašą kurį sudarytų šių sąrašų elementų skirtumai
+
+#4 Nuskaitykite teksto ilutę, pavyzdžiui
+"""
+11A ir 11B klasėse mokosi patys geriaus Šolomo Aleichemo ORT gimnazijos programuotojai!
+"""
+#Padalinkite šią eilutę į atskirus 15 simbolių segmentus, paskutiniame segmente turi likti likutis <= 15.
+#Segmentų skaičius turi priklausyti nuo eilutės ilgio
+#Rezultat1 5ra6ykite į fsailą Result_3.txt, kiekvienas segmentas spausdinamas iš naujos eilutės
+#Rezultato failo pavyzdys
+"""
+11A ir 11B klas
+ėse mokosi paty
+s geriaus Šolom
+o Aleichemo ORT
+gimnazijos prog
+ramuotojai!
+"""
+
+#5 Nuskaitykite vieną teksto teksto eilutę, pavyzdžiui
+"0123456789"
+"0123"
+"6789010"
+"54301"
+
+#Parašykite programą kuri parašytų ar skaičiai eilutėje eina iš eilės, pavyzdžiui:
+TAIP
+TAIP
+TAIP
+NE
