bc69625feb48703c785bc941f78c58ad78d2e473
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-02-10 11:24:03 +0200

    Create Klases_darbas.py

---
+++ 11_2025_2026/21_pamoka/Klases_darbas.py
@@ -0,0 +1,11 @@
+komplektai = [8, 2, 2, 2, 1, 1]
+figuros = [0, 0, 0, 0, 0, 0]
+
+with open('U1.txt', 'r') as file:
+    n = int(file.readline())
+    for _ in range(n):
+        mokinio_figiros = list(map(int, file.readline().strip().split()))
+        for i in range(len(mokinio_figiros)):
+            figuros[i]+=mokinio_figiros[i]
+
+print(figuros)
