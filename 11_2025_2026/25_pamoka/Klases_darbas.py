f6deba3b78196245c42b91b8663368186b2072db
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-05-25 07:56:41 +0300

    Create Klases_darbas.py

---
+++ 11_2025_2026/25_pamoka/Klases_darbas.py
@@ -0,0 +1,18 @@
+# Užduotis
+# Sukurkurkite programą - žodyną, programa paprašytų klaviatūra įvesti kableliu atskirtą žodžių porą
+# (tas pats žodis, tik skirtingomis kalbomis). Pirmasis žodis - žodyno indekssas, antrasis žodyno vertė.
+# Po kiekvieno žodžio programa klaustų ar norite įvesti dar vieną žodžių porą, jei taip leistų įvesti dar vieną žodį
+# ir pridėtų jį, prie jau anksčiau sukurto žodyno, jei ne įrašytų žodyną json formatu į failą
+
+# Programą turi sudaryti bent šios funkcijos:
+# 1. Sekos funkcija, kuri kartojasi tol, kol nepasirenkama, kad nenorite daugiau įvedinėti žodžių
+# 2. Pirmosios funkcijos viduje lokali funkcija kuri atskiria kableliu atskirtus duomenis ir papilo žodyną
+# Ši funkcija turi du argumentus, eilutę (string) kurią sudaro du atskirti kableliu žodžiai ir žodynas
+# Funkcja paima praeitą žodyno versija į jį įterpią naujas vertes ir tuomet grąžina (naudokite lokalius kintamuosius)
+# 3. Funkcija turi du argumentus: užpildytą žodyną ir failo pavadinimą.
+# Ji įrašo žodyną į failą
+
+# Sukurkite programą darbui su ankstesne programa sukurtu failu
+# 4. Sukurkite programą kuri atsidarytų sukurtą json failą iš jo importuotų žodyną ir paklaustų vartotojo iš kurios kalbnos į kurią norėtų versti. 
+# Jeigu pakeičiam kalba atskira funkcija kurios argumentas yra esamas žodynas pasitelkia sąrašų generatoriaus konstrukciją ir grąžina žodyną sukeisdama indeksus su vertėmis.
+# 5. Parašykite atskirą funkcija kuri paprašytų įvesti žodį ir grąžintų reikšmę, taip pat pritaikykite konstrukcija try, except, kad tokiu atveju, kai žodis nerastas parašytų "Nėra tokio žodži
