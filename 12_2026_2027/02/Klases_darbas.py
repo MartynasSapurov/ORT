50acd64fee1c7fa898e1af15448fc5d1df00ef59
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2026-09-24 10:58:10 +0300

    Update Klases_darbas.py

--- 12_2026_2027/02/Klases_darbas.py
+++ 12_2026_2027/02/Klases_darbas.py
@@ -1,3 +1,4 @@
+#1 Užduotis
 Sukurkite klasę turinčią šiuos atributus:
 
 brand, age, cоlor, mark ir weight.
@@ -7,3 +8,14 @@ move, birthday ir stop.
 
 Metodai move ir stop Išveda pranešimus «move» ir «stop», o birthday padidina atributo age vertę.
 Atributus brand, age ir mark būtina nurodyti kuriant objektą.
+
+#2 Užduotis
+
+#1. Sukurkite automobilių klasę kurią sudarys šie atributai: markė, pagaminimo_metai, rida, spalva, galia
+#Kuriant naują objektą būtina nurodyti markę,pagaminimo metus ir ridą
+#2. Sukurkite šiai klasei 4 metodus: arklio_galia() - automobilio galia atspausdinama arklio galiomis (1kW - 1.34 Ag)
+#vaziavimas() - nuskaito nuvažiuotų kilometrų skaičių ir juo padidina ridą ir ją atspausdina
+#horn() - parašo "Pypt"
+#add_atribute() - sukur objektui nauja atributą busena (tvarkinga, nauja, dauzta ....)
+#Sukrkite bent du šios klasės objektus 
+#Sukurkite trečią šios klasės objektą pavdinimu bus ir pridėkitye jam papildomą atributą bus=True
